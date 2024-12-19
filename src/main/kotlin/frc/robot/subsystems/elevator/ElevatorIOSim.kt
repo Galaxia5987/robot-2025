b8fa92f8eb791705b66d51d9a7809a7430ae0661
Emma Livne <emmalivne.03@gmail.com>
2024-12-19 10:45:15 +0200

    Change setHeight to use Distance instead of a double

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOSim.kt
@@ -2,12 +2,13 @@ package frc.robot.subsystems.elevator
 
 import MOTOR_ID
 import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.measure.Distance
 
 class ElevatorIOSim : ElevatorIO {
     override val inputs = LoggedElevatorInputs()
     private val motor = TalonFX(MOTOR_ID)
 
-    override fun setHeight(position: Double) {
+    override fun setHeight(position: Distance) {
         super.setHeight(position)
     }
     override fun setPower(percentOutput: Double) {
