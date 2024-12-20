afb62743ae6f197a529e82f0c3697300d5b445b8
Emma Livne <emmalivne.03@gmail.com>
2024-12-20 12:47:38 +0200

    Change SPROCKET_RADIUS to Double

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -4,10 +4,10 @@ import frc.robot.lib.Gains
 import frc.robot.lib.selectGainsBasedOnMode
 
 const val MAX_HEIGHT = 1.3
-const val SPROCKET_RADIUS= 12.13
-const val GEAR_RATIO = (1 / 12) * (42 / 48)
-const val FIRST_STAGE_RATIO = 0.5
-val GAINS = selectGainsBasedOnMode(Gains(), Gains())
+const val GEAR_RATIO = (1.0 / 12.0) * (42.0 / 48.0)
+const val FIRST_STAGE_RATIO = 2.0
+val SPROCKET_RADIUS: Distance = Units.Millimeters.of(36.4/2.0)
+val GAINS = selectGainsBasedOnMode(
     Gains(
         20.0,
         kD=1.0
