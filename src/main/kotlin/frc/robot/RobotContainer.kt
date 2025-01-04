77488794602824f4f0cc7c50bc69041e41795ff7
Emma Livne <emmalivne.03@gmail.com>
2025-01-04 17:49:47 +0200

    Fix vision initialization

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -33,8 +33,8 @@ object RobotContainer {
         vision = Vision(
             swerveDrive::addVisionMeasurement,
             VisionIOPhotonVision(VisionConstants.FrontOVName, VisionConstants.robotToFrontOV),
-            VisionIOPhotonVision(VisionConstants.LeftOVName, VisionConstants.robotToRightOV),
-            VisionIOPhotonVision(VisionConstants.RightOVName, VisionConstants.robotToLeftOV)
+            VisionIOPhotonVision(VisionConstants.LeftOVName, VisionConstants.robotToLeftOV),
+            VisionIOPhotonVision(VisionConstants.RightOVName, VisionConstants.robotToRightOV)
         )
 
         registerAutoCommands()
