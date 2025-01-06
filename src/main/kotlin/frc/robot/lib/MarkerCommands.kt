5f7b6076a401851775ab5482444b464b26eca503
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-06 12:44:38 +0200

    discard alert usage for an auto logged boolean

--- src/main/kotlin/frc/robot/lib/MarkerCommands.kt
+++ src/main/kotlin/frc/robot/lib/MarkerCommands.kt
@@ -4,15 +4,20 @@ import edu.wpi.first.wpilibj.Alert
 import edu.wpi.first.wpilibj.DataLogManager
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import org.littletonrobotics.junction.AutoLogOutput
+import org.littletonrobotics.junction.Logger
 
 const val ABNORMAL_EVENT_NAME = "PROBLEM!"
 val alert = Alert(ABNORMAL_EVENT_NAME, Alert.AlertType.kWarning)
 
+@AutoLogOutput
+private var marked = false
+
 private fun markEvent(eventName: String): Command = Commands.runOnce({
     DataLogManager.log(eventName)
-    alert.set(true)
+    marked = true
 }).withTimeout(5.0).andThen(Commands.runOnce({
-    alert.set(false)
+    marked = false
 }))
 
 fun markAbnormalEvent(): Command = markEvent(ABNORMAL_EVENT_NAME)
