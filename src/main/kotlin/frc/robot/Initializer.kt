66fb4857d93dbb6ea4213a275b33355eaa8c8919
Emma Livne <emmalivne.03@gmail.com>
2025-01-12 19:50:22 +0200

    Simplify initializer

--- src/main/kotlin/frc/robot/Initializer.kt
+++ src/main/kotlin/frc/robot/Initializer.kt
@@ -1,75 +1,50 @@
 package frc.robot
 
 import frc.robot.generated.TunerConstants
-import frc.robot.subsystems.drive.Drive
-import frc.robot.subsystems.drive.GyroIO
-import frc.robot.subsystems.drive.GyroIONavX
-import frc.robot.subsystems.drive.ModuleIO
-import frc.robot.subsystems.drive.ModuleIOSim
-import frc.robot.subsystems.drive.ModuleIOTalonFX
+import frc.robot.subsystems.drive.*
 import frc.robot.subsystems.vision.Vision
 import frc.robot.subsystems.vision.VisionConstants
-import frc.robot.subsystems.vision.VisionIO
 import frc.robot.subsystems.vision.VisionIOPhotonVision
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim
 
-private var swerveDrive: Drive? = null
-private var vision: Vision? = null
-
-private fun getSwerveModuleIOs(): Array<ModuleIO> {
-    return when (CURRENT_MODE) {
-        Mode.REAL -> arrayOf(
-            ModuleIOTalonFX(TunerConstants.FrontLeft),
-            ModuleIOTalonFX(TunerConstants.FrontRight),
-            ModuleIOTalonFX(TunerConstants.BackLeft),
-            ModuleIOTalonFX(TunerConstants.BackRight)
-        )
-        Mode.SIM -> arrayOf(
-            ModuleIOSim(TunerConstants.FrontLeft),
-            ModuleIOSim(TunerConstants.FrontRight),
-            ModuleIOSim(TunerConstants.BackLeft),
-            ModuleIOSim(TunerConstants.BackRight)
-        )
-        Mode.REPLAY -> arrayOf(
-            object : ModuleIO {},
-            object : ModuleIO {},
-            object : ModuleIO {},
-            object : ModuleIO {}
-        )
+private val swerveModuleIOs =
+    when (CURRENT_MODE) {
+        Mode.REAL ->
+            arrayOf(
+                ModuleIOTalonFX(TunerConstants.FrontLeft),
+                ModuleIOTalonFX(TunerConstants.FrontRight),
+                ModuleIOTalonFX(TunerConstants.BackLeft),
+                ModuleIOTalonFX(TunerConstants.BackRight),
+            )
+        Mode.SIM ->
+            arrayOf(
+                ModuleIOSim(TunerConstants.FrontLeft),
+                ModuleIOSim(TunerConstants.FrontRight),
+                ModuleIOSim(TunerConstants.BackLeft),
+                ModuleIOSim(TunerConstants.BackRight),
+            )
+        Mode.REPLAY -> arrayOf()
     }
-}
 
-private fun getGyroIO(): GyroIO = when (CURRENT_MODE) {
-    Mode.REAL -> GyroIONavX()
-    Mode.SIM -> object : GyroIO {}
-    Mode.REPLAY -> object : GyroIO {}
-}
-
-private fun initSwerve() {
-    if (swerveDrive == null) {
-        swerveDrive = Drive(getGyroIO(), getSwerveModuleIOs())
+private val gyroIO =
+    when (CURRENT_MODE) {
+        Mode.REAL -> GyroIONavX()
+        else -> object : GyroIO {}
     }
-}
 
-fun getSwerve(): Drive {
-    return swerveDrive ?: throw IllegalStateException("Swerve has not been initialized.")
-}
+val swerveDrive = Drive(gyroIO, swerveModuleIOs)
 
-private fun getVisionIOs(): Array<VisionIO> = when (CURRENT_MODE) {
-    Mode.REAL -> VisionConstants.OVNameToTransform.map { VisionIOPhotonVision(it.key, it.value) }.toTypedArray()
-    Mode.SIM -> VisionConstants.OVNameToTransform.map { VisionIOPhotonVisionSim(it.key, it.value, getSwerve()::getPose) }.toTypedArray()
-    Mode.REPLAY -> emptyArray()
-}
+private val visionIOs =
+    when (CURRENT_MODE) {
+        Mode.REAL ->
+            VisionConstants.OVNameToTransform.map {
+                VisionIOPhotonVision(it.key, it.value)
+            }
+        Mode.SIM ->
+            VisionConstants.OVNameToTransform.map {
+                VisionIOPhotonVisionSim(it.key, it.value, swerveDrive::getPose)
+            }
+        Mode.REPLAY -> emptyList()
+    }.toTypedArray()
 
-fun initVision() {
-    vision = Vision(getSwerve()::addVisionMeasurement, *getVisionIOs())
-}
-
-fun getVision(): Vision {
-    return vision ?: throw IllegalStateException("Vision has npt been initialized.")
-}
-
-fun initializeSubsystems() {
-    initSwerve()
-    initVision()
-}
+val vision = Vision(swerveDrive::addVisionMeasurement, *visionIOs)
