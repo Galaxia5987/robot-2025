bb9a4b0e75260403d155cb0f95c6b216709c6ece
Emma <emmalivne.03@gmail.com>
2025-01-14 23:45:15 +0200

    Fix navX IO to work with the update

--- src/main/kotlin/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/kotlin/frc/robot/subsystems/drive/GyroIONavX.java
@@ -17,14 +17,12 @@ import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SerialPort;
 
 import java.util.Queue;
 
 /** IO implementation for NavX. */
 public class GyroIONavX implements GyroIO {
-    private final AHRS navX = new AHRS(SerialPort.Port.kUSB, AHRS.SerialDataType.kProcessedData, (byte) Drive.ODOMETRY_FREQUENCY);
+    private final AHRS navX = new AHRS(NavXComType.kUSB1, (byte) Drive.ODOMETRY_FREQUENCY);
     private final Queue<Double> yawPositionQueue;
     private final Queue<Double> yawTimestampQueue;
 
