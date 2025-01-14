1e30918ecd0ab5d59812f6a25e0cb8b9f7d987f0
noga <noga.veksler@gmail.com>
2025-01-14 16:55:25 +0200

    Delete lines

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -11,10 +11,5 @@ const val GEAR_RATIO = (1.0 / 12.0) * (42.0 / 48.0)
 const val FIRST_STAGE_RATIO = 2.0
 private val SPROCKET_RADIUS: Distance = Units.Millimeters.of(36.4 / 2)
 val ROTATIONS_TO_CENTIMETER = GEAR_RATIO * FIRST_STAGE_RATIO * (SPROCKET_RADIUS.`in`(Units.Centimeter) * 2 * PI)
-val GAINS = selectGainsBasedOnMode(
-    Gains(
-        20.0,
-        kD = 1.0
-    ),
-    Gains()
-)
+val GAINS = selectGainsBasedOnMode(Gains(kP = 20.0, kD = 1.0), Gains())
+
