1f682d93555f38eae81ac3924f4394c977fda660
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 16:20:41 +0200

    Change OUTTAKE_POWER to be negative

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.gripper
 
 const val INTAKE_POWER = 0.3 // TODO: Calibrate
-const val OUTTAKE_POWER = 0.3 // TODO: Calibrate
+const val OUTTAKE_POWER = -0.3 // TODO: Calibrate
 const val GEAR_RATIO = 1.0 // TODO: Replace with real value
 val MOMENT_OF_INERTIA = 0.03 // TODO: Replace with real value
