ec9a2876e126a57ae39e4ac7f8a762579d78391f
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 20:54:18 +0200

    Add pid and config to motors

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
@@ -1,12 +1,22 @@
 package frc.robot.subsystems.climber
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.StrictFollower
 import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
+import frc.robot.subsystems.Port
 
 class ClimberIOReal:ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
-//    var mainMotor = TalonFX()
+    var mainMotor = TalonFX(Port.Climber.mainMotor)
+    var auxMotor = TalonFX(Port.Climber.auxMotor)
+
+    init {
+        auxMotor.setControl(StrictFollower(mainMotor.deviceID))
+        listOf(auxMotor, mainMotor).forEach { it.apply {MOTOR_CONFIG} }
+    }
+
     override fun setLatchPosition(position: Distance) {
 
     }
