059eba92ff8a938afe2ee694c711727e5314c1ce
noga <noga.veksler@gmail.com>
2025-01-15 18:03:54 +0200

    Delete companion object

--- src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
@@ -2,32 +2,11 @@ package frc.robot.subsystems.elevator
 
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
 import java.util.function.DoubleSupplier
 
-class Elevator private constructor(private val io: ElevatorIO) : SubsystemBase() {
-
-
-    companion object {
-        @Volatile
-        private var instance: Elevator? = null
-
-        fun initialize(io: ElevatorIO) {
-            synchronized(this) {
-                if (Elevator.instance == null) {
-                    instance = Elevator(io)
-                }
-            }
-        }
-        fun getInstance(): Elevator {
-            return Elevator.instance ?: throw IllegalStateException(
-                "Elevator has not been initialized. Call initialize(io: ElevatorIO) first."
-            )
-        }
-    }
+class Elevator(private val io: ElevatorIO) : SubsystemBase() {
 
     fun setPosition(position: Distance): Command {
         Logger.recordOutput("Elevator/positionSetpoint",position )
