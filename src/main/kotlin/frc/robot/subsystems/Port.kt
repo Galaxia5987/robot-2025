3d5f7f0024277fe7ede2d6994ab26ebaae393138
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 21:24:02 +0200

    Add more ports

--- src/main/kotlin/frc/robot/subsystems/Port.kt
+++ src/main/kotlin/frc/robot/subsystems/Port.kt
@@ -4,6 +4,8 @@ object Port {
     object Climber {
         const val mainMotor = 0
         const val auxMotor = 1
-        const val Sensor = 2
+        const val servo1 = 2
+        const val servo2 = 3
+        const val Sensor = 4
     }
 }
