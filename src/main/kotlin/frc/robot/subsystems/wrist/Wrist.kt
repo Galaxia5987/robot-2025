3926301ca988869f8eb0fc734f445876327a4d34
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 19:19:06 +0200

    Fix mechanism2d

--- src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
@@ -11,16 +11,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d
 
 class Wrist(private val io: WristIO) : SubsystemBase() {
     @AutoLogOutput
     private var atSetpoint: Trigger = Trigger {
         setpoint.angle.isNear(io.inputs.angle, AT_SETPOINT_TOLERANCE)
     }
-    @AutoLogOutput private val mechanism = Mechanism2d(2.0, 3.0)
+    private val mechanism = LoggedMechanism2d(2.0, 3.0)
     private val root = mechanism.getRoot("Wrist", 1.0, 1.0)
     private val ligament2d =
-        root.append(MechanismLigament2d("WristLigament", 0.21, 0.0))
+        root.append(LoggedMechanismLigament2d("WristLigament", 1.2, 0.0))
 
     @AutoLogOutput private var setpoint = Angles.ZERO
 
@@ -55,5 +57,7 @@ class Wrist(private val io: WristIO) : SubsystemBase() {
             )
         )
         ligament2d.setAngle(io.inputs.angle.`in`(Units.Degrees))
+        Logger.recordOutput("Wrist/Mechanism2d", mechanism)
+        Logger.recordOutput("Wrist/Setpoint", setpoint)
     }
 }
