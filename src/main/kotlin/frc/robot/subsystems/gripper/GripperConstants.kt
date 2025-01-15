5aa852b69a9a526767e11edc7cf7182a94063eb9
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 15:29:01 +0200

    Create GEAR_RATIO constant

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -2,3 +2,4 @@ package frc.robot.subsystems.gripper
 
 const val INTAKE_POWER = 0.3 // TODO: Calibrate
 const val OUTTAKE_POWER = 0.3 // TODO: Calibrate
+const val GEAR_RATIO = 1 // TODO: Replace with real value
