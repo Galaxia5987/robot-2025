8da3775275f15c749fafdd1733d69b78fbf652ed
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 19:55:31 +0200

    Create WristIOReal

---
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristIOReal.kt
@@ -0,0 +1,67 @@
+package frc.robot.subsystems.wrist
+
+import com.ctre.phoenix6.configs.*
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.CANcoder
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.GravityTypeValue
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue
+import edu.wpi.first.units.measure.Angle
+
+class WristIOReal : WristIO {
+    override val inputs = LoggedWristInputs()
+    private val positionControl = PositionVoltage(0.0)
+    private val dutyCycle = DutyCycleOut(0.0)
+
+    private val motor: TalonFX = TalonFX(MOTOR_PORT)
+    private val absoluteEncoder = CANcoder(CANCODER_PORT)
+
+    init {
+        motor.configurator.apply(TalonFXConfiguration().apply {
+            MotorOutput = MotorOutputConfigs().apply {
+                NeutralMode = NeutralModeValue.Brake
+                Inverted = InvertedValue.Clockwise_Positive
+            }
+            Feedback = FeedbackConfigs().apply {
+                RotorToSensorRatio = 1.0
+                SensorToMechanismRatio = GEAR_RATIO
+            }
+            Slot0 = Slot0Configs().apply {
+                kP = 1.0
+                kI = 0.0
+                kD = 0.0
+                kG = 0.0
+                GravityType = GravityTypeValue.Arm_Cosine
+                StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign
+            }
+            CurrentLimits = CurrentLimitsConfigs().apply {
+                StatorCurrentLimitEnable = true
+                SupplyCurrentLimitEnable = true
+                StatorCurrentLimit = 40.0
+                SupplyCurrentLimit = 80.0
+            }
+        })
+    }
+
+    override fun setAngle(angle: Angle) {
+        motor.setControl(positionControl.withPosition(angle))
+    }
+
+    override fun setPower(power: Double) {
+        motor.setControl(dutyCycle.withOutput(power))
+    }
+
+    override fun resetAbsoluteEncoder(angle: Angle) {
+        absoluteEncoder.setPosition(angle)
+    }
+
+    override fun updateInputs() {
+        inputs.angle.mut_replace(motor.position.value)
+        inputs.appliedVoltage.mut_replace(motor.motorVoltage.value)
+        inputs.absoluteEncoderAngle.mut_replace(absoluteEncoder.position.value)
+        inputs.noOffsetAbsoluteEncoderPosition.mut_replace(absoluteEncoder.position.value)
+    }
+}
