b4ba00ce9d13a56f929407e587ed15fbe546d5d2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 16:59:05 +0200

    Add inputs val

--- src/main/kotlin/frc/robot/subsystems/wrist/WristIO.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristIO.kt
@@ -5,6 +5,8 @@ import edu.wpi.first.units.measure.*
 import org.team9432.annotation.Logged
 
 interface WristIO {
+    val inputs: LoggedWristInputs
+
     fun setAngle(angle: Angle) {}
     fun setPower(power: Double) {}
     fun resetAbsoluteEncoder() {}
