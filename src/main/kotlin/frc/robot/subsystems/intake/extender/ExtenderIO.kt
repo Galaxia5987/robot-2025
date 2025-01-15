858a5164554dedfba918762c9c19465143f34469
Emma <emmalivne.03@gmail.com>
2025-01-15 02:01:48 +0200

    Add ExtenderIO

---
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIO.kt
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.intake.extender
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.units.measure.Voltage
+import org.team9432.annotation.Logged
+
+interface ExtenderIO {
+    val inputs: LoggedExtenderInputs
+
+    fun setPosition(position: Distance) {}
+
+    fun setPower(power: Double) {}
+
+    fun updateInputs() {}
+
+    @Logged
+    open class ExtenderInputs {
+        var appliedVoltage: Voltage = Units.Volts.of(0.0)
+        var position: Distance = Units.Meters.of(0.0)
+        var sensedDistance: Distance = Units.Meters.of(0.0)
+    }
+}
