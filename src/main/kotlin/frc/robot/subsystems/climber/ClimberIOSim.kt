ef43de3b32b75355f88b5ffba17c20d8ae54c598
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 16:56:08 +0200

    Create Climber io sim

---
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.climber
+
+import frc.robot.lib.motors.TalonFXSim
+
+class ClimberIOSim:ClimberIO {
+    override var inputs: LoggedInputClimber = LoggedInputClimber()
+
+    override fun setLatchPosition() {
+    }
+
+    override fun setPower() {
+    }
+
+    override fun setAngle() {
+    }
+}
