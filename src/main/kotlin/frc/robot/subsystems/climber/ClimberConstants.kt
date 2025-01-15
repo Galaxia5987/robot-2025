ee8938a3a403b4f1609bba09ed01be7c2a471878
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 20:40:09 +0200

    Add pid to constants

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -4,6 +4,8 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.MomentOfInertia
+import frc.robot.lib.Gains
+import frc.robot.lib.selectGainsBasedOnMode
 
 const val UNFOLD_POWER = 1
 const val FOLD_POWER = 1
@@ -11,7 +13,16 @@ val OPEN_LATCH_POSITION: Distance = Units.Centimeter.of(0.8)
 val CLOSE_LATCH_POSITION: Distance = Units.Centimeter.of(0.2)
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
 val FOLDED_ANGLE: Angle = Units.Degree.of(30.0)
-const val GEAR_RATIO = 1.0
-val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
-var DISTANCE_THRESHOLD: Distance = Units.Centimeter.of(0.4)
-var LATCH_TOLERANCE: Distance = Units.Centimeter.of(0.4)
+const val gearRation = 1.0
+val momentOfInertia: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
+var distanceThreshold: Distance = Units.Centimeter.of(0.4)
+var latchTolerance: Distance = Units.Centimeter.of(0.4)
+var Gains = selectGainsBasedOnMode(
+    Gains(
+        0.0,
+        0.0,
+    ), Gains(
+        0.0,
+        0.0,
+    )
+)
