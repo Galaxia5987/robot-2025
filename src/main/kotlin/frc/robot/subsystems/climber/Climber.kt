822544e6def8b5be59326459623dd6a5dceba363
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 19:23:55 +0200

    Add lock unlock fold and unfold

--- src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
@@ -1,71 +1,47 @@
 package frc.robot.subsystems.climber
 
+import edu.wpi.first.networktables.DoubleEntry
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.units.measure.Dimensionless
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import org.littletonrobotics.junction.AutoLogOutput
-import org.littletonrobotics.junction.Logger
 
-class Climber(private val io: ClimberIO) : SubsystemBase() {
+class Climber private constructor(private val io: ClimberIO) : SubsystemBase() {
     var inputs = io.inputs
 
     @AutoLogOutput
-    private var isTouching = Trigger {
-        inputs.sensorDistance < DISTANCE_THRESHOLD
+    private var isTouching = Trigger { inputs.sensorDistance.lt(distanceThreshold) }
+    private val hasClimbed = Trigger { inputs.angle.lt(FOLDED_ANGLE) }
+    private var isLatchClosed = Trigger { inputs.latchPosition.lt(latchTolerance) }
+    private var isAttached = Trigger(isLatchClosed.and(isTouching))
+
+    companion object {
+        @Volatile
+        private var instance: Climber? = null
+
+        fun initialize(io: ClimberIO) {
+            synchronized(this) {
+                if (instance == null) {
+                    instance = Climber(io)
+                }
+            }
+        }
+
+        fun getInstance(): Climber {
+            return instance ?: throw IllegalStateException(
+                "Climber has not been initialized. Call initialize(io: ClimberIO) first."
+            )
+        }
     }
-    @AutoLogOutput
-    private var isLatchClosed = Trigger {
-        inputs.latchPosition.isNear(CLOSE_LATCH_POSITION, LATCH_TOLERANCE)
-    }
+    fun closeLatch():Command = Commands.runOnce({io.setLatchPosition(CLOSE_LATCH_POSITION)})
+    fun openLatch():Command = Commands.runOnce({io.setLatchPosition(OPEN_LATCH_POSITION)})
+    fun lock():Command = Commands.runOnce({io.lock()})
+    fun unlock():Command = Commands.runOnce({io.unlock()})
+    fun unfold(){} //TODO
+    fun fold(){}   //TODO
+    private fun setAngle(angle:Angle):Command = Commands.runOnce({io.setAngle(angle)})
+    private fun setPower(power:Double):Command = Commands.runOnce({io.setPower(power)})
 
-    @AutoLogOutput
-    private var isAttached = Trigger(isLatchClosed).and(isTouching)
-
-    @AutoLogOutput
-    private val isFolded = Trigger {
-        inputs.angle.isNear(FOLDED_ANGLE, FOLDED_TOLERANCE)
-    }
-
-    fun closeLatch(): Command = runOnce({ setLatchPose(CLOSE_LATCH_POSITION) })
-
-    private fun setLatchPose(latchPose: Dimensionless) =
-        io.setLatchPosition(latchPose)
-
-    fun openLatch(): Command = runOnce({ setLatchPose(OPEN_LATCH_POSITION) })
-
-    fun lock(): Command = runOnce({ io.lock() })
-    fun unlock(): Command = runOnce({ io.unlock() })
-    fun unfold() {
-        io.setAngle(UNFOLDED_ANGLE)
-    }
-
-    fun fold() {
-        io.setAngle(FOLDED_ANGLE)
-    }
-
-    fun climb(): Command =
-        run { setLatchPose(CLOSE_LATCH_POSITION) }
-            .until(isLatchClosed)
-            .andThen(setAngle(FOLDED_ANGLE))
-            .until(isFolded)
-            .andThen(lock())
-
-    fun unClimb(): Command =
-        run { setLatchPose(CLOSE_LATCH_POSITION) }
-            .andThen(unlock())
-            .andThen({ unfold() })
-            .andThen(openLatch())
-
-    private fun setAngle(angle: Angle): Command =
-        runOnce({ io.setAngle(angle) })
-
-    private fun setPower(power: Double): Command =
-        runOnce({ io.setPower(power) })
-
-    override fun periodic() {
-        io.updateInput()
-        Logger.processInputs(this::class.simpleName, io.inputs)
-    }
 }
