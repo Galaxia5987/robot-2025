732fb9e35ff3f99e7d7fb12511de3e51e9044f72
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 19:19:41 +0200

    Create WristIOSim

---
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristIOSim.kt
@@ -0,0 +1,40 @@
+package frc.robot.subsystems.wrist
+
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.controls.PositionVoltage
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.wpilibj.Timer
+import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
+
+class WristIOSim : WristIO {
+    override val inputs = LoggedWristInputs()
+
+    private val motor =
+        TalonFXSim(1, GEAR_RATIO, MOMENT_OF_INERTIA, 1.0, TalonType.FALCON)
+    private val angleController = PIDController(1.0, 0.0, 0.0)
+    private val positionControl = PositionVoltage(0.0)
+    private val dutyCycle = DutyCycleOut(0.0)
+
+    init {
+        motor.setController(angleController)
+    }
+
+    override fun setAngle(angle: Angle) {
+        motor.setControl(positionControl.withPosition(angle))
+    }
+
+    override fun setPower(power: Double) {
+        motor.setControl(dutyCycle.withOutput(power))
+    }
+
+    override fun resetAbsoluteEncoder() {}
+
+    override fun updateInputs() {
+        motor.update(Timer.getFPGATimestamp())
+        inputs.angle.mut_replace(motor.position, Units.Rotations)
+        inputs.appliedVoltage.mut_replace(motor.appliedVoltage, Units.Volts)
+    }
+}
