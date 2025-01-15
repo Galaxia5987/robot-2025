91058e03072d9556c65ebbb0796c1f6215ef336f
Emma Livne <emmalivne.03@gmail.com>
2025-01-15 17:58:26 +0200

    Process inputs

--- src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake.roller
 
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.Logger
 
 class Roller(private val io: RollerIO) : SubsystemBase() {
 
@@ -16,5 +17,6 @@ class Roller(private val io: RollerIO) : SubsystemBase() {
 
     override fun periodic() {
         io.updateInputs()
+        Logger.processInputs("Intake/Roller", io.inputs)
     }
 }
