76d51620d96613b1b89b9fad7e3a211343c67b48
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 17:35:27 +0200

    Create WristConstants.kt

---
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.wrist
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+
+val AT_SETPOINT_TOLERANCE: Angle = Units.Degrees.of(1.0)
+
+enum class Angles(val angle: Angle) {
+    L1(Units.Degrees.of(90.0)),
+    L2(Units.Degrees.of(35.0)),
+    L3(Units.Degrees.of(35.0)),
+    L4(Units.Degrees.of(90.0)),
+    L2_ALGAE(Units.Degrees.of(110.0)),
+    L3_ALGAE(Units.Degrees.of(110.0)),
+    FEEDER(Units.Degrees.of(125.0)),
+    ZERO(Units.Degrees.zero())
+}
