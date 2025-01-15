cb5708a2fcdb137741ab440b90613d1cee74bd2c
noga <noga.veksler@gmail.com>
2025-01-15 22:42:28 +0200

    Update usage of TalonFXSim

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOSim.kt
@@ -7,14 +7,24 @@ import edu.wpi.first.math.system.plant.LinearSystemId
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
 
 class ElevatorIOSim : ElevatorIO {
     override val inputs = LoggedElevatorInputs()
     private val motorPosititonRequest = PositionVoltage(0.0)
-    private val motor = TalonFXSim(
-        LinearSystemId.createElevatorSystem(DCMotor.getKrakenX60(2), WEIGHT, 36.4 / 2, GEAR_RATIO), 2,
-        GEAR_RATIO, 1.0
-    )
+    private val motor =
+        TalonFXSim(
+            LinearSystemId.createElevatorSystem(
+                DCMotor.getKrakenX60(2),
+                WEIGHT,
+                36.4 / 2,
+                GEAR_RATIO
+            ),
+            2,
+            GEAR_RATIO,
+            1.0,
+            TalonType.KRAKEN
+        )
 
     override fun setHeight(position: Distance) {
         val rotationalPosition = Units.Rotations.of(position.`in`(Units.Centimeter) / ROTATIONS_TO_CENTIMETER)
