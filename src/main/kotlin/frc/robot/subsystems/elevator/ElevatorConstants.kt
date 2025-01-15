e1e4d0df1fcf872e4a7de10c9ac17241a66c5434
noga <noga.veksler@gmail.com>
2025-01-15 17:28:46 +0200

    Change MAX_HEIGHT

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -6,7 +6,7 @@ import frc.robot.lib.Gains
 import frc.robot.lib.selectGainsBasedOnMode
 import kotlin.math.PI
 
-const val MAX_HEIGHT = 1.3
+val MAX_HEIGHT: Distance = Units.Meters.of(1.3)
 const val GEAR_RATIO = (1.0 / 12.0) * (42.0 / 48.0)
 const val FIRST_STAGE_RATIO = 2.0
 private val SPROCKET_RADIUS: Distance = Units.Millimeters.of(36.4 / 2)
