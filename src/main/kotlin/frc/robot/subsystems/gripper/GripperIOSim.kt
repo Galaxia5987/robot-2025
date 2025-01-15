da8f4d469d8ab065a95cbbc12e7655e856c4b66d
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 15:41:59 +0200

    Create GripperIOSim

---
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperIOSim.kt
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.gripper
+
+class GripperIOSim : GripperIO {
+    override val inputs: LoggedGripperInputs
+        get() = TODO("Not yet implemented")
+
+    override fun setPower(power: Double) {
+        super.setPower(power)
+    }
+
+    override fun updateInputs() {
+        super.updateInputs()
+    }
+}
