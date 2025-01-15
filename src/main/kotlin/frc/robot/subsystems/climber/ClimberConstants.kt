d2bef3565071dd4724ec81c098b73b83d15aa131
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 20:55:02 +0200

    Changed constants to upper case letters

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -2,20 +2,16 @@ package frc.robot.subsystems.climber
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.units.measure.Dimensionless
+import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.MomentOfInertia
 
 const val UNFOLD_POWER = 1
 const val FOLD_POWER = 1
-val OPEN_LATCH_POSITION: Dimensionless = Units.Percent.of(0.8)
-val CLOSE_LATCH_POSITION = Units.Percent.of(0.2)
-val UNLOCK_ANGLE = Units.Degree.of(90.0)
-val LOCK_ANGLE = Units.Degree.of(10.0)
+val OPEN_LATCH_POSITION: Distance = Units.Centimeter.of(0.8)
+val CLOSE_LATCH_POSITION: Distance = Units.Centimeter.of(0.2)
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
 val FOLDED_ANGLE: Angle = Units.Degree.of(30.0)
 const val GEAR_RATIO = 1.0
-val FOLDED_TOLERANCE = Units.Degree.of(1.0)
-val MOMENT_OF_INERTIA_MAIN: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
-val MOMENT_OF_INERTIA_LOCK: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
-var DISTANCE_THRESHOLD = Units.Centimeter.of(0.4)
-val LATCH_TOLERANCE: Dimensionless = Units.Percent.of(0.03)
+val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
+var DISTANCE_THRESHOLD: Distance = Units.Centimeter.of(0.4)
+var LATCH_TOLERANCE: Distance = Units.Centimeter.of(0.4)
