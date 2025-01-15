dab3044486681620b1d36264303ed1a42bfe0a97
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 15:36:50 +0200

    Add inputs to GripperIO

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperIO.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperIO.kt
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.gripper
 
+import edu.wpi.first.units.Units
+import org.team9432.annotation.Logged
+
 interface GripperIO {
+    val inputs: LoggedGripperInputs
+
     fun setPower(power: Double) {}
+
+    fun updateInputs() {}
+
+    @Logged
+    open class GripperInputs {
+        var appliedVoltage = Units.Volts.zero()
+    }
 }
