70a5b120b7260457e134ca1aa8cca964b349367e
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 17:56:44 +0200

    Add vars motor dutyCycle and positionControler

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -1,16 +1,26 @@
 package frc.robot.subsystems.climber
 
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.controls.PositionVoltage
+import edu.wpi.first.units.Units
 import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
 
-class ClimberIOSim:ClimberIO {
+class ClimberIOSim : ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
+    var motor = TalonFXSim(2, gearRation, momentOfInertia.`in`(Units.KilogramSquareMeters), 1.0, TalonType.KRAKEN)
+    var dutyCycle = DutyCycleOut(0.0)
+    var positionControler =PositionVoltage(0.0)
 
     override fun setLatchPosition() {
+
     }
 
-    override fun setPower() {
+    override fun setPower(power:Double) {
+        motor.setControl(dutyCycle.withOutput(power))
     }
 
     override fun setAngle() {
+
     }
 }
