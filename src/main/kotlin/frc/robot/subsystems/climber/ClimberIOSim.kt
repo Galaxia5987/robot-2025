c332702f44e75d2c0ce7c2a3087187425e22f60b
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 17:58:34 +0200

    Add vars to pass in those functions

---
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.climber
+
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.controls.PositionVoltage
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Distance
+import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
+
+class ClimberIOSim : ClimberIO {
+    override var inputs: LoggedInputClimber = LoggedInputClimber()
+    var motor = TalonFXSim(2, gearRation, momentOfInertia.`in`(Units.KilogramSquareMeters), 1.0, TalonType.KRAKEN)
+    var dutyCycle = DutyCycleOut(0.0)
+    var positionControler =PositionVoltage(0.0)
+
+    override fun setLatchPosition(position: Distance) {
+
+    }
+
+    override fun setPower(power:Double) {
+        motor.setControl(dutyCycle.withOutput(power))
+    }
+
+    override fun setAngle(angle: Angle) {
+        motor.setControl(positionControler.withPosition(angle))
+    }
+
+}
