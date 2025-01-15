09f50461a8ef382e3332f31200ffbd01da75004d
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 20:55:02 +0200

    Changed constants to upper case letters

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -2,12 +2,13 @@ package frc.robot.subsystems.climber
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.MomentOfInertia
 
 const val UNFOLD_POWER = 1
 const val FOLD_POWER = 1
-const val OPEN_LATCH_POSITION = 0.8
-const val CLOSE_LATCH_POSITION = 0.2
+val OPEN_LATCH_POSITION: Distance = Units.Centimeter.of(0.8)
+val CLOSE_LATCH_POSITION: Distance = Units.Centimeter.of(0.2)
 val UNLOCK_ANGLE = Units.Degree.of(90.0)
 val LOCK_ANGLE = Units.Degree.of(10.0)
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
@@ -16,4 +17,4 @@ const val GEAR_RATIO = 1.0
 val MOMENT_OF_INERTIA_MAIN: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
 val MOMENT_OF_INERTIA_LOCK: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
 var DISTANCE_THRESHOLD = Units.Centimeter.of(0.4)
-var LATCH_TOLERANCE = 0.03
+var LATCH_TOLERANCE: Distance = Units.Centimeter.of(0.4)
