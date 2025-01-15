37492f55144867c4558a6a00013dab0d66035dd0
noga <noga.veksler@gmail.com>
2025-01-15 20:13:44 +0200

    Add AutoLog

--- src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
@@ -4,11 +4,13 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
 import java.util.function.DoubleSupplier
 
 class Elevator(private val io: ElevatorIO) : SubsystemBase() {
 
+    @AutoLogOutput
     var positionSetpoint: Distance = Units.Millimeters.of(0.0)
 
     fun setPosition(position: Distance): Command {
