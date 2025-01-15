f2214f0cefc080f4578ed10fba5c58ab102d4d42
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 21:30:55 +0200

    Add servo to code

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.climber
 
+import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.DutyCycleOut
 import com.ctre.phoenix6.controls.PositionVoltage
@@ -7,11 +8,14 @@ import com.ctre.phoenix6.controls.StrictFollower
 import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.wpilibj.Servo
 import frc.robot.subsystems.Port
 
 class ClimberIOReal:ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
     var mainMotor = TalonFX(Port.Climber.mainMotor)
+    var servo1 = Servo(Port.Climber.servo1)
+    var servo2 = Servo(Port.Climber.servo2)
     var auxMotor = TalonFX(Port.Climber.auxMotor)
     var dutyCycleOut = DutyCycleOut(0.0)
     var positionVoltage = PositionVoltage(0.0)
@@ -21,8 +25,8 @@ class ClimberIOReal:ClimberIO {
         listOf(auxMotor, mainMotor).forEach { it.apply {MOTOR_CONFIG} }
     }
 
-    override fun setLatchPosition(position: Distance) {
-
+    override fun setLatchPosition(position: Double) {
+//        listOf(servo2,servo1).forEach{it.set()}
     }
 
     override fun setPower(power: Double) {
