457bc31738b7847fccbf3bb459d0eb7ba9c3d25a
Emma Livne <emmalivne.03@gmail.com>
2025-01-15 17:23:59 +0200

    Add motorCurrent input

--- src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIO.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIO.kt
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.intake.extender
 
 import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Current
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
@@ -19,6 +20,7 @@ interface ExtenderIO {
     @Logged
     open class ExtenderInputs {
         var appliedVoltage: Voltage = Units.Volts.of(0.0)
+        var motorCurrent: Current = Units.Amps.of(0.0)
         var position: Distance = Units.Meters.of(0.0)
         var sensorValue: Distance = Units.Meters.of(0.0)
     }
