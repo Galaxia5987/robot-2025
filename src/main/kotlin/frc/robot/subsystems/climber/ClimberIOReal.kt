5ac710acf397bf70b088da88c7b5b6547d0d9d95
yahali david mor <mor.yahali1612@gmail.com>
2025-01-15 21:20:59 +0200

    Add Control request to set power and angle

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.climber
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.controls.StrictFollower
 import com.ctre.phoenix6.hardware.TalonFX
 import edu.wpi.first.units.measure.Angle
@@ -11,6 +13,8 @@ class ClimberIOReal:ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
     var mainMotor = TalonFX(Port.Climber.mainMotor)
     var auxMotor = TalonFX(Port.Climber.auxMotor)
+    var dutyCycleOut = DutyCycleOut(0.0)
+    var positionVoltage = PositionVoltage(0.0)
 
     init {
         auxMotor.setControl(StrictFollower(mainMotor.deviceID))
@@ -22,10 +26,11 @@ class ClimberIOReal:ClimberIO {
     }
 
     override fun setPower(power: Double) {
+        mainMotor.setControl(dutyCycleOut.withOutput(power))
     }
 
     override fun setAngle(angle: Angle) {
-
+        mainMotor.setControl(positionVoltage.withPosition(angle))
     }
 
     override fun lock() {
@@ -33,7 +38,6 @@ class ClimberIOReal:ClimberIO {
     }
 
     override fun unlock() {
-        super.unlock()
     }
 
     override fun updateInput() {
