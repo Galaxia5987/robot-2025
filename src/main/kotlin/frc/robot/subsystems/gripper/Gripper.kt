f3472b2762f37b66830a1be0bc5773fca645c320
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-15 15:37:36 +0200

    Create Gripper Singleton

---
+++ src/main/kotlin/frc/robot/subsystems/gripper/Gripper.kt
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.gripper
+
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.Logger
+
+class Gripper private constructor(private val io: GripperIO) : SubsystemBase() {
+    companion object {
+        @Volatile
+        private var instance: Gripper? = null
+
+        fun initialize(io: GripperIO) {
+            synchronized(this) {
+                if (instance == null) {
+                    instance = Gripper(io)
+                }
+            }
+        }
+
+        fun getInstance(): Gripper {
+            return instance ?: throw IllegalArgumentException(
+                "Gripper has not been initialized. Call initialize(io: GripperIO) first."
+            )
+        }
+    }
+}
