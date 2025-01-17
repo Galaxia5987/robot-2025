a163c3836572568aaa94c7d5753eeffeb2280515
David Masin <davidmasin2910@gmail.com>
2025-01-17 11:30:49 +0200

    Updated Gripper Changed folders' names

---
+++ src/main/kotlin/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "robot-2025-2";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 248;
+  public static final String GIT_SHA = "70534bdd8b04ea64e7d55ede554af7e52d04bfb8";
+  public static final String GIT_DATE = "2025-01-17 11:04 IST";
+  public static final String GIT_BRANCH = "feature/block-bot-as-vis";
+  public static final String BUILD_DATE = "2025-01-17 11:10 IST";
+  public static final long BUILD_UNIX_TIME = 1737105049034L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
