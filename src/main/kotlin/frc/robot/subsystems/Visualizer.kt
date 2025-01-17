96db87b165cf27f6100be0a841acb81b555e2fdc
Dan <dan.dan.kafe@gmail.com>
2025-01-17 14:37:01 +0200

    Add climber angle getter parameter

--- src/main/kotlin/frc/robot/subsystems/Visualizer.kt
+++ src/main/kotlin/frc/robot/subsystems/Visualizer.kt
@@ -10,7 +10,8 @@ import edu.wpi.first.units.measure.Distance
 class Visualizer(
     private val elevatorHeight: () -> Distance,
     private val wristAngle: () -> Angle,
-    private val extenderPosition: () -> Distance
+    private val extenderPosition: () -> Distance,
+    private val climberAngle: () -> Angle,
 ) {
     private fun getElevatorPoses(): Pair<Pose3d, Pose3d> {
         val carriageHeight = elevatorHeight.invoke().`in`(Meters)
@@ -24,5 +25,12 @@ class Visualizer(
     private fun getWristPose(carriagePose: Pose3d): Pose3d {
         val wristAngleDouble = wristAngle.invoke().`in`(Radians)
         return carriagePose.rotateBy(Rotation3d(0.0, wristAngleDouble, 0.0))
+    }
+
+    fun visualize(): Array<Pose3d> {
+        val intakePose = Pose3d(extenderPosition.invoke().`in`(Meters), 0.0, 0.0, Rotation3d.kZero)
+        val (firstStagePose, secondStagePose) = getElevatorPoses()
+        val wristPose = getWristPose(secondStagePose)
+        return arrayOf(intakePose, firstStagePose, secondStagePose, wristPose)
     }
 }
