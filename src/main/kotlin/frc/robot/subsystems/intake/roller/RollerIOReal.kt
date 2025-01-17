ca15d3678e115d6cd86e22a42aae709e814e4ba1
Emma <emmalivne.03@gmail.com>
2025-01-17 02:02:16 +0200

    Add real IO

---
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerIOReal.kt
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.intake.roller
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs
+import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
+
+class RollerIOReal : RollerIO {
+    override val inputs = LoggedRollerInputs()
+    private val motor = TalonFX(MOTOR_ID)
+    private val controlRequest = DutyCycleOut(0.0)
+
+    init {
+        val motorConfig = TalonFXConfiguration().apply {
+            MotorOutput = MotorOutputConfigs().apply {
+                Inverted = InvertedValue.Clockwise_Positive
+                NeutralMode = NeutralModeValue.Brake
+            }
+            CurrentLimits = CurrentLimitsConfigs().apply {
+                StatorCurrentLimitEnable = true
+                SupplyCurrentLimitEnable = true
+                StatorCurrentLimit = 80.0
+                SupplyCurrentLimit = 40.0
+            }
+        }
+    }
+
+    override fun setPower(power: Double) {
+        super.setPower(power)
+    }
+}
