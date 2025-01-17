a57cd5a8dd350bc6c0bd67c7bcf1b0b9f0971b69
Dan <dan.dan.kafe@gmail.com>
2025-01-17 14:12:48 +0200

    Add function that returns the elevator poses

--- src/main/kotlin/frc/robot/subsystems/Visualizer.kt
+++ src/main/kotlin/frc/robot/subsystems/Visualizer.kt
@@ -1,5 +1,8 @@
 package frc.robot.subsystems
 
+import edu.wpi.first.math.geometry.Pose3d
+import edu.wpi.first.math.geometry.Rotation3d
+import edu.wpi.first.units.Units.Meters
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 
@@ -8,4 +11,12 @@ class Visualizer(
     private val wristAngle: () -> Angle,
     private val extenderPosition: () -> Distance
 ) {
+    private fun getElevatorPoses(): Pair<Pose3d, Pose3d> {
+        val carriageHeight = elevatorHeight.invoke().`in`(Meters)
+        val firstStageHeight = carriageHeight / 2.0
+
+        val firstStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
+        val secondStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
+        return Pair(firstStagePose, secondStagePose)
+    }
 }
