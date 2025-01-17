4bb08eedec7cb394468720770c4a22605190ab21
Emma <emmalivne.03@gmail.com>
2025-01-17 02:07:27 +0200

    Add constants for sim IO

--- src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderConstants.kt
@@ -6,6 +6,8 @@ import edu.wpi.first.units.measure.Distance
 
 const val GEAR_RATIO = 0.0
 const val RESET_POWER = 0.0
+val MASS = Units.Kilogram.of(1.5)
+val PINION_RADIUS = Units.Millimeters.of(15.22)
 val RESET_CURRENT_THRESHOLD: Current = Units.Amps.of(0.0)
 val POSITION_TOLERANCE: Distance = Units.Meters.of(0.0)
 val EXTENDED_POSITION: Distance = Units.Meters.of(0.0)
