bc5982ebcce2c4234a3c4394bbaf7dedf4c310d6
yahali david mor <mor.yahali1612@gmail.com>
2025-01-17 15:53:05 +0200

    Add fold unfold climb and unClimb

--- src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
@@ -35,13 +35,36 @@ class Climber private constructor(private val io: ClimberIO) : SubsystemBase() {
         }
     }
 
-    fun closeLatch(): Command = runOnce({ io.setLatchPosition(CLOSE_LATCH_POSITION) })
-    fun openLatch(): Command = runOnce({ io.setLatchPosition(OPEN_LATCH_POSITION) })
+    fun closeLatch(): Command =
+        runOnce({ io.setLatchPosition(CLOSE_LATCH_POSITION) })
+
+    fun openLatch(): Command =
+        runOnce({ io.setLatchPosition(OPEN_LATCH_POSITION) })
+
     fun lock(): Command = runOnce({ io.lock() })
     fun unlock(): Command = runOnce({ io.unlock() })
-    fun unfold() {}
-    fun fold(){}
-    private fun setAngle(angle: Angle): Command = runOnce({ io.setAngle(angle) })
-    private fun setPower(power: Double): Command = runOnce({ io.setPower(power) })
+    fun unfold() {
+        io.setAngle(UNFOLDED_ANGLE)
+    }
 
+    fun fold() {
+        io.setAngle(FOLDED_ANGLE)
+    }
+
+    fun climb(): Command =
+        run { closeLatch() }
+            .andThen(setAngle(FOLDED_ANGLE).onlyIf(isLatchClosed))
+            .finallyDo(lock().onlyIf(isFolded))
+
+    fun unClimb(): Command =
+        run { setPower(-0.4) }
+            .andThen(unlock())
+            .andThen({ unfold() })
+            .finallyDo(openLatch())
+
+    private fun setAngle(angle: Angle): Command =
+        runOnce({ io.setAngle(angle) })
+
+    private fun setPower(power: Double): Command =
+        runOnce({ io.setPower(power) })
 }
