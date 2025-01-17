2d1026b0384c22078b952c93aec2c171e31bdc6c
yahali david mor <mor.yahali1612@gmail.com>
2025-01-17 14:38:04 +0200

    Add servo as solenoid

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -2,21 +2,28 @@ package frc.robot.subsystems.climber
 
 import com.ctre.phoenix6.controls.DutyCycleOut
 import com.ctre.phoenix6.controls.PositionVoltage
+import com.revrobotics.servohub.ServoHubSim
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj.simulation.SolenoidSim
 import frc.robot.lib.motors.TalonFXSim
 import frc.robot.lib.motors.TalonType
 
 class ClimberIOSim : ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
     var motor = TalonFXSim(2, GEAR_RATIO, MOMENT_OF_INERTIA.`in`(Units.KilogramSquareMeters), 1.0, TalonType.KRAKEN)
+    var servo = SolenoidSim(PneumaticsModuleType.REVPH, 0)
     var dutyCycle = DutyCycleOut(0.0)
-    var positionControler =PositionVoltage(0.0)
+    var positionControler = PositionVoltage(0.0)
 
-    override fun setLatchPosition(position: Distance) {
-
+    override fun setLatchPosition(position: Double) {
+        if (Double.equals(OPEN_LATCH_POSITION)) {
+            servo.output = true
+        } else {
+            servo.output = false
+        }
     }
 
     override fun lock() {
@@ -27,7 +34,7 @@ class ClimberIOSim : ClimberIO {
         //TODO
     }
 
-    override fun setPower(power:Double) {
+    override fun setPower(power: Double) {
         motor.setControl(dutyCycle.withOutput(power))
     }
 
@@ -37,7 +44,7 @@ class ClimberIOSim : ClimberIO {
 
     override fun updateInput() {
         motor.update(Timer.getFPGATimestamp())
-        inputs.angle =  Units.Rotations.of(motor.position)
+        inputs.angle = Units.Rotations.of(motor.position)
         inputs.appliedVoltage = Units.Volt.of(motor.appliedVoltage)
     }
 }
