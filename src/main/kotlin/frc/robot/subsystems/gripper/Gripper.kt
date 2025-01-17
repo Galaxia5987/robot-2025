6f84fe09eb330816e002dad77e9ecc61b3828d61
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-17 16:17:27 +0200

    Remove singleton

--- src/main/kotlin/frc/robot/subsystems/gripper/Gripper.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/Gripper.kt
@@ -5,27 +5,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
 
 class Gripper private constructor(private val io: GripperIO) : SubsystemBase() {
-    companion object {
-        @Volatile private var instance: Gripper? = null
-
-        fun initialize(io: GripperIO) {
-            synchronized(this) {
-                if (instance == null) {
-                    instance = Gripper(io)
-                }
-            }
-        }
-
-        fun getInstance(): Gripper {
-            return instance
-                ?: throw IllegalArgumentException(
-                    "Gripper has not been initialized. Call initialize(io: GripperIO) first."
-                )
-        }
-    }
-
     fun intake(): Command =
         runOnce { io.setPower(INTAKE_POWER) }.withName("GripperIntake")
+
     fun outtake(): Command =
         runOnce { io.setPower(OUTTAKE_POWER) }.withName("GripperOuttake")
 
