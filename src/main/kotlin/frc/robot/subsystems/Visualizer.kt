7d574173c0128f0d1239bf5e8a3c90adf1541d5c
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-01-18 21:30:20 +0200

    Bring back class
    Something about passing seven suppliers in every loop makes me uneasy. I'm also not sure `@AutoLogOutput` would work that way.

--- src/main/kotlin/frc/robot/subsystems/Visualizer.kt
+++ src/main/kotlin/frc/robot/subsystems/Visualizer.kt
@@ -8,42 +8,43 @@ import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 import org.littletonrobotics.junction.AutoLogOutput
 
+class Visualizer(
+    private val extenderPosition: () -> Distance,
+    private val intakeRollerAngle: () -> Angle,
+    private val elevatorHeight: () -> Distance,
+    private val wristAngle: () -> Angle,
+    private val coralRollersAngle: () -> Angle,
+    private val algaeRemoverAngle: () -> Angle,
+    private val climberAngle: () -> Angle,
+) {
+    private fun getElevatorPoses(): Pair<Pose3d, Pose3d> {
+        val carriageHeight = elevatorHeight.invoke().`in`(Meters)
+        val firstStageHeight = carriageHeight / 2.0
 
-private fun getElevatorPoses(elevatorHeight: () -> Distance): Pair<Pose3d, Pose3d> {
-    val carriageHeight = elevatorHeight.invoke().`in`(Meters)
-    val firstStageHeight = carriageHeight / 2.0
+        val firstStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
+        val secondStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
+        return Pair(firstStagePose, secondStagePose)
+    }
 
-    val firstStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
-    val secondStagePose = Pose3d(0.0, 0.0, firstStageHeight, Rotation3d.kZero)
-    return Pair(firstStagePose, secondStagePose)
-}
+    @AutoLogOutput
+    fun visualizeSubsystems(): Array<Pose3d> {
+        val intakePose = Pose3d(extenderPosition.invoke().`in`(Meters), 0.0, 0.0, Rotation3d.kZero)
+        val intakeRollerPose = intakePose.rotateBy(Rotation3d(0.0, intakeRollerAngle.invoke().`in`(Radians), 0.0))
 
-@AutoLogOutput
-fun visualize(
-    extenderPosition: () -> Distance,
-    intakeRollerAngle: () -> Angle,
-    elevatorHeight: () -> Distance,
-    wristAngle: () -> Angle,
-    coralRollersAngle: () -> Angle,
-    algaeRemoverAngle: () -> Angle,
-    climberAngle: () -> Angle,
-): Array<Pose3d> {
-    val intakePose = Pose3d(extenderPosition.invoke().`in`(Meters), 0.0, 0.0, Rotation3d.kZero)
-    val intakeRollerPose = intakePose.rotateBy(Rotation3d(0.0, intakeRollerAngle.invoke().`in`(Radians), 0.0))
+        val (firstStagePose, secondStagePose) = getElevatorPoses()
+        val wristPose = secondStagePose.rotateBy(Rotation3d(0.0, wristAngle.invoke().`in`(Radians), 0.0))
 
-    val (firstStagePose, secondStagePose) = getElevatorPoses(elevatorHeight)
-    val wristPose = secondStagePose.rotateBy(Rotation3d(0.0, wristAngle.invoke().`in`(Radians), 0.0))
+        val coralRollersPose = wristPose.rotateBy(Rotation3d(0.0, coralRollersAngle.invoke().`in`(Radians), 0.0))
+        val algaeRemoverPose = wristPose.rotateBy(Rotation3d(0.0, algaeRemoverAngle.invoke().`in`(Radians), 0.0))
 
-    val coralRollersPose = wristPose.rotateBy(Rotation3d(0.0, coralRollersAngle.invoke().`in`(Radians), 0.0))
-    val algaeRemoverPose = wristPose.rotateBy(Rotation3d(0.0, algaeRemoverAngle.invoke().`in`(Radians), 0.0))
+        val climberPose = Pose3d(0.0, 0.0, 0.0, Rotation3d(0.0, climberAngle.invoke().`in`(Radians), 0.0))
 
-    val climberPose = Pose3d(0.0, 0.0, 0.0, Rotation3d(0.0, climberAngle.invoke().`in`(Radians), 0.0))
-
-    return arrayOf(
-        intakePose, intakeRollerPose,
-        firstStagePose, secondStagePose,
-        // The two identical poses are for the upper and lower rollers.
-        wristPose, coralRollersPose, coralRollersPose, algaeRemoverPose,
-        climberPose
-    )
+        return arrayOf(
+            intakePose, intakeRollerPose,
+            firstStagePose, secondStagePose,
+            // The two identical poses are for the upper and lower rollers.
+            wristPose, coralRollersPose, coralRollersPose, algaeRemoverPose,
+            climberPose
+        )
+    }
 }
