f707ba4e4f543219269b618aeaa41245925bf4c3
yahali david mor <mor.yahali1612@gmail.com>
2025-01-18 19:54:11 +0200

    spotlessApply

--- src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
@@ -5,7 +5,6 @@ import edu.wpi.first.units.measure.Dimensionless
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
-import frc.robot.lib.finallyDo
 import org.littletonrobotics.junction.AutoLogOutput
 import org.littletonrobotics.junction.Logger
 
@@ -18,21 +17,23 @@ class Climber(private val io: ClimberIO) : SubsystemBase() {
     }
     @AutoLogOutput
     private var isLatchClosed = Trigger {
-        inputs.latchPosition.isNear(CLOSE_LATCH_POSITION,LATCH_TOLERANCE)
+        inputs.latchPosition.isNear(CLOSE_LATCH_POSITION, LATCH_TOLERANCE)
     }
 
     @AutoLogOutput
     private var isAttached = Trigger(isLatchClosed).and(isTouching)
+
     @AutoLogOutput
-    private val isFolded = Trigger { inputs.angle.isNear(FOLDED_ANGLE, FOLDED_TOLERANCE)  }
+    private val isFolded = Trigger {
+        inputs.angle.isNear(FOLDED_ANGLE, FOLDED_TOLERANCE)
+    }
 
-    fun closeLatch(): Command =
-        runOnce({ setLatchPose(CLOSE_LATCH_POSITION) })
+    fun closeLatch(): Command = runOnce({ setLatchPose(CLOSE_LATCH_POSITION) })
 
-    private fun setLatchPose(latchPose: Dimensionless) = io.setLatchPosition(latchPose)
+    private fun setLatchPose(latchPose: Dimensionless) =
+        io.setLatchPosition(latchPose)
 
-    fun openLatch(): Command =
-        runOnce({ setLatchPose(OPEN_LATCH_POSITION) })
+    fun openLatch(): Command = runOnce({ setLatchPose(OPEN_LATCH_POSITION) })
 
     fun lock(): Command = runOnce({ io.lock() })
     fun unlock(): Command = runOnce({ io.unlock() })
@@ -45,14 +46,14 @@ class Climber(private val io: ClimberIO) : SubsystemBase() {
     }
 
     fun climb(): Command =
-        run {setLatchPose(CLOSE_LATCH_POSITION)}
+        run { setLatchPose(CLOSE_LATCH_POSITION) }
             .until(isLatchClosed)
             .andThen(setAngle(FOLDED_ANGLE))
             .until(isFolded)
             .andThen(lock())
 
     fun unClimb(): Command =
-        run {setLatchPose(CLOSE_LATCH_POSITION)}
+        run { setLatchPose(CLOSE_LATCH_POSITION) }
             .andThen(unlock())
             .andThen({ unfold() })
             .andThen(openLatch())
