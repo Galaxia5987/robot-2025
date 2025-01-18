54c95594ca5608eb96dbf78af713a3144562d54f
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-18 17:07:37 +0200

    Refactor `setPower` & `setPosition` to be more concise

--- src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
@@ -12,14 +12,12 @@ class Elevator(private val io: ElevatorIO) : SubsystemBase() {
 
     @AutoLogOutput var positionSetpoint: Distance = Units.Millimeters.of(0.0)
 
-    fun setPosition(position: Distance): Command {
+    fun setPosition(position: Distance): Command = runOnce{
         positionSetpoint = position
-        return run { io.setHeight(position) }
+        io.setHeight(position)
     }
 
-    fun setPower(percentOutput: DoubleSupplier): Command {
-        return run { io.setPower(percentOutput.asDouble) }
-    }
+    fun setPower(percentOutput: DoubleSupplier): Command = run { io.setPower(percentOutput.asDouble)}
 
     fun resetAbsoluteEncoder(): Command = runOnce(io::resetAbsoluteEncoder)
 
