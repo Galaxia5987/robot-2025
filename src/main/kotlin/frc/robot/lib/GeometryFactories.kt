9c3a2f699d567895379c903eb3a527d0390407e7
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-01-18 18:46:48 +0200

    Add `Pose2d` factories

--- src/main/kotlin/frc/robot/lib/GeometryFactories.kt
+++ src/main/kotlin/frc/robot/lib/GeometryFactories.kt
@@ -1,5 +1,6 @@
 package frc.robot.lib
 
+import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.units.Units.Meters
@@ -13,3 +14,12 @@ fun getTranslation2d(x: Distance = Meters.zero(), y: Distance = Meters.zero()) =
 fun getRotation2d(x: Double = 0.0, y: Double = 0.0) = Rotation2d(x, y)
 
 fun getRotation2d(x: Distance = Meters.zero(), y: Distance = Meters.zero()) = Rotation2d(x.`in`(Meters), y.`in`(Meters))
+
+
+fun getPose2d(translation: Translation2d = Translation2d(), rotation: Rotation2d = Rotation2d()) =
+    Pose2d(translation, rotation)
+
+fun getPose2d(x: Double = 0.0, y: Double = 0.0, rotation: Rotation2d = Rotation2d()) = Pose2d(x, y, rotation)
+
+fun getPose2d(x: Distance = Meters.zero(), y: Distance = Meters.zero(), rotation: Rotation2d = Rotation2d()) =
+    Pose2d(x, y, rotation)
