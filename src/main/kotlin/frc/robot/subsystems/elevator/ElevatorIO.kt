bebcb9cf6197680b8c88e00d3bacf62a883c6dee
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-18 16:57:24 +0200

    Fix typo in applied voltage + spotless

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIO.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIO.kt
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.elevator
 
 import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
@@ -20,6 +21,8 @@ interface ElevatorIO {
     open class ElevatorInputs {
         var height: Distance = Units.Meters.of(0.0)
         var setpoint: Distance = Units.Meters.of(0.0)
-        var appliedVoltege: Voltage = Units.Volts.of(0.0)
+        var appliedVoltage: Voltage = Units.Volts.of(0.0)
+        var noOffsetAbsoluteEncoderPosition: Angle = Units.Rotations.of(0.0)
+        var absoluteEncoderHeight: Distance = Units.Meters.of(0.0)
     }
 }
