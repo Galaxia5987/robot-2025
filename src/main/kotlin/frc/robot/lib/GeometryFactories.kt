1bb4b5c60290f2724f310e3fdbd5f977d1d25892
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-01-18 18:38:26 +0200

    Add `Translation2d` factories

--- src/main/kotlin/frc/robot/lib/GeometryFactories.kt
+++ src/main/kotlin/frc/robot/lib/GeometryFactories.kt
@@ -1,1 +1,9 @@
 package frc.robot.lib
+
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.units.Units.Meters
+import edu.wpi.first.units.measure.Distance
+
+fun getTranslation2d(x: Double = 0.0, y: Double = 0.0) = Translation2d(x, y)
+
+fun getTranslation2d(x: Distance = Meters.zero(), y: Distance = Meters.zero()) = Translation2d(x, y)
