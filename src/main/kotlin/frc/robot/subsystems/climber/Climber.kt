552bc94c405c3e4a253992feec2ecad560f7dfaf
yahali david mor <mor.yahali1612@gmail.com>
2025-01-18 18:45:14 +0200

    Change latchposition to Dimention less

--- src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
@@ -1,20 +1,27 @@
 package frc.robot.subsystems.climber
 
+import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Dimensionless
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.lib.finallyDo
 import org.littletonrobotics.junction.AutoLogOutput
 
 class Climber private constructor(private val io: ClimberIO) : SubsystemBase() {
     var inputs = io.inputs
 
     @AutoLogOutput
-    private var isTouching = Trigger { inputs.sensorDistance.lt(DISTANCE_THRESHOLD) }
+    private var isTouching = Trigger {
+        inputs.sensorDistance.lt(DISTANCE_THRESHOLD)
+    }
     private val hasClimbed = Trigger { inputs.angle.lt(FOLDED_ANGLE) }
-    private var isLatchClosed = Trigger { inputs.latchPosition < LATCH_TOLERANCE + CLOSE_LATCH_POSITION }
+    private var isLatchClosed = Trigger {
+        inputs.latchPosition < LATCH_TOLERANCE + CLOSE_LATCH_POSITION
+    }
     private var isAttached = Trigger(isLatchClosed.and(isTouching))
+    private val isFolded = Trigger { inputs.angle == FOLDED_ANGLE }
 
     companion object {
         @Volatile
@@ -29,19 +36,43 @@ class Climber private constructor(private val io: ClimberIO) : SubsystemBase() {
         }
 
         fun getInstance(): Climber {
-            return instance ?: throw IllegalStateException(
-                "Climber has not been initialized. Call initialize(io: ClimberIO) first."
-            )
+            return instance
+                ?: throw IllegalStateException(
+                    "Climber has not been initialized. Call initialize(io: ClimberIO) first."
+                )
         }
     }
 
-    fun closeLatch(): Command = runOnce({ io.setLatchPosition(CLOSE_LATCH_POSITION) })
-    fun openLatch(): Command = runOnce({ io.setLatchPosition(OPEN_LATCH_POSITION) })
+    fun closeLatch(): Command =
+        runOnce({ io.setLatchPosition(CLOSE_LATCH_POSITION) })
+
+    fun openLatch(): Command =
+        runOnce({ io.setLatchPosition(OPEN_LATCH_POSITION) })
+
     fun lock(): Command = runOnce({ io.lock() })
     fun unlock(): Command = runOnce({ io.unlock() })
-    fun unfold() {}
-    fun fold(){}
-    private fun setAngle(angle: Angle): Command = runOnce({ io.setAngle(angle) })
-    private fun setPower(power: Double): Command = runOnce({ io.setPower(power) })
+    fun unfold() {
+        io.setAngle(UNFOLDED_ANGLE)
+    }
 
+    fun fold() {
+        io.setAngle(FOLDED_ANGLE)
+    }
+
+    fun climb(): Command =
+        run { closeLatch() }
+            .andThen(setAngle(FOLDED_ANGLE).onlyIf(isLatchClosed))
+            .finallyDo(lock().onlyIf(isFolded))
+
+    fun unClimb(): Command =
+        run { setPower(-0.4) }
+            .andThen(unlock())
+            .andThen({ unfold() })
+            .finallyDo(openLatch())
+
+    private fun setAngle(angle: Angle): Command =
+        runOnce({ io.setAngle(angle) })
+
+    private fun setPower(power: Double): Command =
+        runOnce({ io.setPower(power) })
 }
--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -2,16 +2,16 @@ package frc.robot.subsystems.climber
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.units.measure.Dimensionless
 import edu.wpi.first.units.measure.MomentOfInertia
 
 const val UNFOLD_POWER = 1
 const val FOLD_POWER = 1
-val OPEN_LATCH_POSITION: Distance = Units.Centimeter.of(0.8)
-val CLOSE_LATCH_POSITION: Distance = Units.Centimeter.of(0.2)
+val OPEN_LATCH_POSITION: Dimensionless = Units.Percent.of(0.8)
+val CLOSE_LATCH_POSITION = Units.Percent.of(0.2)
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
 val FOLDED_ANGLE: Angle = Units.Degree.of(30.0)
 const val GEAR_RATIO = 1.0
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
-var DISTANCE_THRESHOLD: Distance = Units.Centimeter.of(0.4)
-var LATCH_TOLERANCE: Distance = Units.Centimeter.of(0.4)
+var DISTANCE_THRESHOLD = Units.Centimeter.of(0.4)
+var LATCH_TOLERANCE: Dimensionless = Units.Percent.of(0.03)
