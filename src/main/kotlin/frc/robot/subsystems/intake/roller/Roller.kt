dc8b7cd615ddccbcae4117d4a94eca44a3d93773
Emma <emmalivne.03@gmail.com>
2025-01-19 00:54:16 +0200

    Change setPower to a runEnd command

--- src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
@@ -6,9 +6,11 @@ import org.littletonrobotics.junction.Logger
 
 class Roller(private val io: RollerIO) : SubsystemBase() {
 
-    private fun setPower(power: Double): Command = runOnce {
-        io.setPower(power)
-    }
+    private fun setPower(power: Double): Command =
+        runEnd(
+            {io.setPower(power)},
+            {io.setPower(0.0)}
+        ).withName("roller/setPower")
 
     fun intake() = setPower(INTAKE_POWER)
 
