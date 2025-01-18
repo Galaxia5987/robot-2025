58c984570636cca3e513580cc648dc37ae4fe1e4
yahali david mor <mor.yahali1612@gmail.com>
2025-01-18 18:30:41 +0200

    Add sensor to code

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOReal.kt
@@ -1,30 +1,30 @@
 package frc.robot.subsystems.climber
 
 import com.ctre.phoenix.motorcontrol.ControlMode
-import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
-import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.DutyCycleOut
 import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.controls.StrictFollower
 import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.math.filter.MedianFilter
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
-import edu.wpi.first.units.measure.Distance
-import edu.wpi.first.wpilibj.Servo
+import edu.wpi.first.wpilibj.AnalogInput
 import frc.robot.lib.motors.LinearServo
 
 
 class ClimberIOReal:ClimberIO {
     override var inputs: LoggedInputClimber = LoggedInputClimber()
     val mainMotor = TalonFX(MAIN_MOTOR_ID)
-    val servo1 = LinearServo(SERVO_1_ID,1,1)
-    val servo2 = LinearServo(SERVO_2_ID,1,1)
+    val servo1 = LinearServo(SERVO_1_ID, 1, 1)
+    val servo2 = LinearServo(SERVO_2_ID, 1, 1)
     val auxMotor = TalonFX(AUX_MOTOR_ID)
     val lockServo = TalonSRX(LOCK_MOTOR_ID)
     val dutyCycleOut = DutyCycleOut(0.0)
     val positionVoltage = PositionVoltage(0.0)
-
+    val sensor = AnalogInput(SENSOR_ID)
+    private val distanceFilter = MedianFilter(3)
+    private val distance = AnalogInput(0)
 
     init {
         auxMotor.setControl(StrictFollower(mainMotor.deviceID))
@@ -56,5 +56,11 @@ class ClimberIOReal:ClimberIO {
         inputs.angle = mainMotor.position.value
         inputs.appliedVoltage = mainMotor.supplyVoltage.value
         inputs.latchPosition = servo1.position
+        var calculatedDistance = distanceFilter.calculate(4800 / (200 * distance.getVoltage() - 20.0))
+        if (calculatedDistance < 0) {
+            calculatedDistance = 80.0
+        }
+
+        inputs.sensorDistance = Units.Meters.of(calculatedDistance)
     }
 }
