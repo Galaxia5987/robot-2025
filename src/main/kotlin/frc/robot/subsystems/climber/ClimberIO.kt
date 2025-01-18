304e42f7e360a2c31a18fad3bcf35a6a72a15e82
yahali david mor <mor.yahali1612@gmail.com>
2025-01-18 18:45:14 +0200

    Change latchposition to Dimention less

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIO.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIO.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climber
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Dimensionless
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
@@ -19,7 +20,7 @@ interface ClimberIO {
     open class InputClimber {
         var appliedVoltage: Voltage = Units.Volts.zero()
         var angle: Angle = Units.Degree.zero()
-        var latchPosition: Double = 0.0
+        var latchPosition: Dimensionless = Units.Percent.of(0.0)
         var sensorDistance: Distance = Units.Centimeter.zero()
     }
 }
