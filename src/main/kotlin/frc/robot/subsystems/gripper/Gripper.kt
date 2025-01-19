7e4ddbdc848bcad0c81a0339d040eb4f8531db2d
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-19 17:21:31 +0200

    Change `setVoltage` Command to be `startEnd`

--- src/main/kotlin/frc/robot/subsystems/gripper/Gripper.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/Gripper.kt
@@ -6,14 +6,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
 
 class Gripper(private val io: GripperIO) : SubsystemBase() {
-    private fun setVoltage(voltage: Voltage): Command = runOnce {
-        io.setVoltage(voltage)
-    }
+    private fun setVoltage(voltage: Voltage): Command =
+        startEnd({ io.setVoltage(voltage) }, { io.setVoltage(STOP_VOLTAGE) })
 
     fun intake(): Command =
         setVoltage(INTAKE_VOLTAGE).withName("Gripper/Intake")
+
     fun outtake(): Command =
         setVoltage(OUTTAKE_VOLTAGE).withName("Gripper/Outtake")
+
     fun removeAlgae(): Command =
         setVoltage(REMOVE_ALGAE_VOLTAGE).withName("Gripper/RemoveAlgae")
 
--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -9,5 +9,6 @@ const val GEAR_RATIO = 1.0 // TODO: Replace with real value
 val INTAKE_VOLTAGE: Voltage = Units.Volts.of(0.3) // TODO: Calibrate
 val OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(-0.3) // TODO: Calibrate
 val REMOVE_ALGAE_VOLTAGE: Voltage = Units.Volts.of(0.3) // TODO: Calibrate
+val STOP_VOLTAGE = Units.Volts.zero()
 val MOMENT_OF_INERTIA: MomentOfInertia =
     Units.KilogramSquareMeters.of(0.03) // TODO: Replace with real value
