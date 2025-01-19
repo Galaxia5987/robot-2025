fd9aa52fca92831d1b3aaa4215c9d17a04144233
Emma <emmalivne.03@gmail.com>
2025-01-19 22:56:36 +0200

    Add LinearServo class

--- src/main/kotlin/frc/robot/lib/motors/LinearServo.java
+++ src/main/kotlin/frc/robot/lib/motors/LinearServo.java
@@ -10,32 +10,64 @@ public class LinearServo extends Servo {
     double setPos;
     double curPos;
 
+    /**
+     * Parameters for L16-R Actuonix Linear Actuators
+     *
+     * @param channel PWM channel used to control the servo
+     * @param length max length of the servo [mm]
+     * @param speed max speed of the servo [mm/second]
+     */
     public LinearServo(int channel, int length, int speed) {
         super(channel);
-//        setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+        setBoundsMicroseconds(2, 2, 1, 1, 1);
         m_length = length;
         m_speed = speed;
     }
-
-    public void setPosition(double setpoint) {
+    
+    /**
+     Miniature Linear Servo Actuators -
+     User Guide (Rev 1)
+     Page 10Table of Contentswcproducts.com
+     * Run this method in any periodic function to update the position estimation of your
+     servo
+     *
+     * @param setpoint the target position of the servo [mm]
+     */
+    public void setPosition(double setpoint){
         setPos = MathUtil.clamp(setpoint, 0, m_length);
-        setSpeed((setPos / m_length * 2) - 1);
+        setSpeed( (setPos/m_length *2)-1);
     }
-
     double lastTime = 0;
-
-    public void updateCurPos() {
+    /**
+     * Run this method in any periodic function to update the position estimation of your
+     servo
+     */
+    public void updateCurPos(){
         double dt = Timer.getFPGATimestamp() - lastTime;
-        if (curPos > setPos + m_speed * dt) {
-            curPos -= m_speed * dt;
-        } else if (curPos < setPos - m_speed * dt) {
-            curPos += m_speed * dt;
-        } else {
+        if (curPos > setPos + m_speed *dt){
+            curPos -= m_speed *dt;
+        } else if(curPos < setPos - m_speed *dt){
+            curPos += m_speed *dt;
+        }else{
             curPos = setPos;
         }
     }
-
-    public double getPosition() {
+    /**
+     * Current position of the servo, must be calling {@link #updateCurPos()
+    updateCurPos()} periodically
+     *
+     * @return Servo Position [mm]
+     */
+    public double getPosition(){
         return curPos;
+    }
+
+    /**
+     * Checks if the servo is at its target position, must be calling {@link #updateCurPos()
+    updateCurPos()} periodically
+     * @return true when servo is at its target
+     */
+    public boolean isFinished(){
+        return curPos == setPos;
     }
 }
