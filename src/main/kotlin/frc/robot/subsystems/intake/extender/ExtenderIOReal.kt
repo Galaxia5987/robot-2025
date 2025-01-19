f79001736d9b3bde76e00c396ccf67b56a94a874
Emma Livne <emmalivne.03@gmail.com>
2025-01-19 21:49:42 +0200

    Add slot0 configs

--- src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIOReal.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake.extender
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC
 import com.ctre.phoenix6.controls.VoltageOut
@@ -30,6 +31,16 @@ class ExtenderIOReal : ExtenderIO {
                         SupplyCurrentLimitEnable = true
                         StatorCurrentLimit = 80.0
                         SupplyCurrentLimit = 40.0
+                    }
+
+                Slot0 =
+                    Slot0Configs().apply {
+                        kP = GAINS.kP
+                        kI = GAINS.kI
+                        kD = GAINS.kD
+                        kV = GAINS.kV
+                        kA = GAINS.kA
+                        kS = GAINS.kS
                     }
             }
         motor.configurator.apply(motorConfig)
