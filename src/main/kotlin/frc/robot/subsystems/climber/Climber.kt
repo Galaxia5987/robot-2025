93ca8729a6427ebab5437e4924423f7b6c2432fe
Emma <emmalivne.03@gmail.com>
2025-01-20 00:43:18 +0200

    Reorder functions and fix some logic

--- src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/Climber.kt
@@ -3,6 +3,7 @@ package frc.robot.subsystems.climber
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import org.littletonrobotics.junction.AutoLogOutput
@@ -28,40 +29,42 @@ class Climber(private val io: ClimberIO) : SubsystemBase() {
         inputs.angle.isNear(FOLDED_ANGLE, FOLDED_TOLERANCE)
     }
 
-    fun closeLatch(): Command = runOnce({ setLatchPose(CLOSE_LATCH_POSITION) }).until(isLatchClosed)
-
-    private fun setLatchPose(latchPose: Angle) =
-        io.setLatchPosition(latchPose)
-
-    fun openLatch(): Command = runOnce({ setLatchPose(OPEN_LATCH_POSITION) })
-
-    fun lock(): Command = runOnce({ io.lock() })
-
-    fun unlock(): Command = runOnce({ io.unlock() })
-
-    fun unfold() = io.setAngle(UNFOLDED_ANGLE)
-
-    fun fold() = io.setAngle(FOLDED_ANGLE)
-
-    fun climb() {
-        run({ closeLatch() })
-            .until(isLatchClosed)
-            .andThen({ fold() })
-            .until(isFolded)
-            .andThen(lock())
-    }
-
-    fun unClimb(): Command =
-        run { setLatchPose(CLOSE_LATCH_POSITION) }
-            .andThen(unlock())
-            .andThen({ unfold() })
-            .andThen(openLatch())
-
     private fun setAngle(angle: Angle): Command =
-        runOnce({ io.setAngle(angle) })
+        runOnce { io.setAngle(angle) }
 
     private fun setPower(power: Double): Command =
-        runOnce({ io.setPower(power) })
+        Commands.startEnd({ io.setPower(power) }, {io.setPower(0.0)})
+
+    private fun setLatchPose(latchPose: Angle): Command =
+        runOnce { io.setLatchPosition(latchPose) }
+
+    fun closeLatch(): Command = setLatchPose(CLOSE_LATCH_POSITION)
+
+    fun openLatch(): Command = setLatchPose(OPEN_LATCH_POSITION)
+
+    fun lock(): Command = runOnce{ io.closeStopper() }
+
+    fun unlock(): Command =
+        setPower(UNLOCK_POWER).withTimeout(0.15)
+            .andThen( {io.openStopper()} )
+
+    fun unfold() = setAngle(UNFOLDED_ANGLE)
+
+    fun fold() = setAngle(FOLDED_ANGLE)
+
+    fun climb(): Command =
+        Commands.sequence(
+            closeLatch(),
+            fold(),
+            lock()
+        )
+
+    fun unClimb(): Command =
+        Commands.sequence(
+            unlock(),
+            unfold(),
+            openLatch()
+        )
 
     override fun periodic() {
         io.updateInput()
