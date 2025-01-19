d473d9123793cbc750d14da622481180b91dcaff
Emma Livne <emmalivne.03@gmail.com>
2025-01-19 17:45:36 +0200

    Apply spotless

--- src/main/kotlin/frc/robot/lib/ExtensionFunctions.kt
+++ src/main/kotlin/frc/robot/lib/ExtensionFunctions.kt
@@ -11,9 +11,9 @@ import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.WrapperCommand
 import frc.robot.IS_RED
+import org.littletonrobotics.junction.LogTable
 import kotlin.math.PI
 import kotlin.math.hypot
-import org.littletonrobotics.junction.LogTable
 
 fun ChassisSpeeds.getSpeed() = hypot(vxMetersPerSecond, vyMetersPerSecond)
 
@@ -98,14 +98,12 @@ fun Rotation2d.flipIfNeeded(): Rotation2d = if (IS_RED) this.flip() else this
 
 fun Distance.toAngle(radius: Distance, gearRatio: Double): Angle =
     this.timesConversionFactor(
-        Units.Rotations.per(Units.Meters).of(
-            1.0 / radius.`in`(Units.Meters) * gearRatio * 2.0 * PI
-        )
+        Units.Rotations.per(Units.Meters)
+            .of(1.0 / radius.`in`(Units.Meters) * gearRatio * 2.0 * PI)
     )
 
 fun Angle.toDistance(radius: Distance, gearRatio: Double): Distance =
     this.timesConversionFactor(
-        Units.Meters.per(Units.Rotations).of(
-            radius.`in`(Units.Meters) * gearRatio * 2.0 * PI
-        )
+        Units.Meters.per(Units.Rotations)
+            .of(radius.`in`(Units.Meters) * gearRatio * 2.0 * PI)
     )
