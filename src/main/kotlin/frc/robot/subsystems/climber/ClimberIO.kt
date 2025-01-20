0803705315fb26400e9fdca7185f8fd888a88e44
Emma Livne <emmalivne.03@gmail.com>
2025-01-20 15:10:20 +0200

    Add stopperMotorCurrent input

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIO.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIO.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climber
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Current
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
@@ -18,6 +19,7 @@ interface ClimberIO {
     @Logged
     open class ClimberInputs {
         var appliedVoltage: Voltage = Units.Volts.zero()
+        var stopperMotorCurrent: Current = Units.Amps.zero()
         var angle: Angle = Units.Degree.zero()
         var latchPosition: Distance = Units.Millimeters.zero()
         var sensorDistance: Distance = Units.Centimeter.zero()
