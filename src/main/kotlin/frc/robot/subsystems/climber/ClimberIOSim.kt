192090f1d9f4d29dc720cf9c676e70a40609df8f
Emma Livne <emmalivne.03@gmail.com>
2025-01-20 14:42:04 +0200

    Use kraken foc

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -24,7 +24,7 @@ class ClimberIOSim : ClimberIO {
             GEAR_RATIO,
             MOMENT_OF_INERTIA.`in`(Units.KilogramSquareMeters),
             1.0,
-            TalonType.KRAKEN
+            TalonType.KRAKEN_FOC
         )
     private var servo1 = SolenoidSim(PneumaticsModuleType.REVPH, LATCH_SERVO_ID)
     private var servo2 = SolenoidSim(PneumaticsModuleType.REVPH, FOLLOW_LATCH_SERVO_ID)
