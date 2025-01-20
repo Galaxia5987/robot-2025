a2b92c39ee170b2e206618cc9eb959ddaef573d3
Emma Livne <emmalivne.03@gmail.com>
2025-01-20 15:41:28 +0200

    Use TalonFXSim for stopper motor

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberIOSim.kt
@@ -3,14 +3,12 @@ package frc.robot.subsystems.climber
 import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.controls.VoltageOut
 import edu.wpi.first.math.system.plant.DCMotor
-import edu.wpi.first.math.system.plant.LinearSystemId
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.Timer
-import edu.wpi.first.wpilibj.simulation.DCMotorSim
 import edu.wpi.first.wpilibj.simulation.SolenoidSim
 import frc.robot.lib.motors.TalonFXSim
 import frc.robot.lib.motors.TalonType
@@ -29,13 +27,11 @@ class ClimberIOSim : ClimberIO {
     private var servo1 = SolenoidSim(PneumaticsModuleType.REVPH, LATCH_SERVO_ID)
     private var servo2 = SolenoidSim(PneumaticsModuleType.REVPH, FOLLOW_LATCH_SERVO_ID)
     private val stopperMotor =
-        DCMotorSim(
-            LinearSystemId.createDCMotorSystem(
-                DCMotor.getBag(1),
-                MOMENT_OF_INERTIA_LOCK.`in`(Units.KilogramSquareMeters),
-                1.0
-            ),
-            DCMotor.getBag(1)
+        TalonFXSim(
+            DCMotor.getBag(1),
+            STOPPER_GEAR_RATIO,
+            MOMENT_OF_INERTIA_LOCK.`in`(Units.KilogramSquareMeters),
+            1.0
         )
     private var voltageControl = VoltageOut(0.0)
     private var positionController = PositionVoltage(0.0)
@@ -49,11 +45,11 @@ class ClimberIOSim : ClimberIO {
     }
 
     override fun closeStopper() {
-        stopperMotor.setAngle(LOCK_ANGLE.`in`(Units.Radians))
+        stopperMotor.setControl(voltageControl.withOutput(LOCK_POWER * 12))
     }
 
     override fun openStopper() {
-        stopperMotor.setAngle(UNLOCK_ANGLE.`in`(Units.Radians))
+        stopperMotor.setControl(voltageControl.withOutput(UNLOCK_POWER * 12))
     }
 
     override fun setVoltage(voltage: Voltage) {
