7a762b166aa5e0e95ae95603a0b84ece824126f2
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-20 15:54:32 +0200

    Add Logging to all subsystems poses

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -6,6 +6,7 @@ package frc.robot
 import edu.wpi.first.hal.FRCNetComm.tInstances
 import edu.wpi.first.hal.FRCNetComm.tResourceType
 import edu.wpi.first.hal.HAL
+import edu.wpi.first.math.geometry.Pose3d
 import edu.wpi.first.wpilibj.Compressor
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.PneumaticsModuleType
@@ -104,6 +105,7 @@ object Robot : LoggedRobot() {
      */
     override fun robotPeriodic() {
         CommandScheduler.getInstance().run()
+        Logger.recordOutput("SubsystemPoses", Pose3d(), *RobotContainer.getVisualizerPoses())
     }
 
     /**
