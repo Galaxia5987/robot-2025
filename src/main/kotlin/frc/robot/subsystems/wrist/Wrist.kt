491b3433c0c6a40f0f9dcc3b77366e766340d7df
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-20 10:50:26 +0200

    Add `angle` supplier in Wrist

--- src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
@@ -28,6 +28,8 @@ class Wrist(private val io: WristIO) : SubsystemBase() {
         setpointValue.isNear(io.inputs.angle, AT_SETPOINT_TOLERANCE)
     }
 
+    var angle: () -> Angle = { io.inputs.angle }
+
     private fun setVoltage(voltage: Voltage): Command = runOnce {
         io.setVoltage(voltage)
     }
