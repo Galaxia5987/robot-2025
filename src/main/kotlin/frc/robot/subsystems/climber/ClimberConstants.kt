1b5766050c3cae8bb3490f90f4bb85f3529c9b3f
Emma Livne <emmalivne.03@gmail.com>
2025-01-20 13:42:21 +0200

    Add type declarations

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -4,13 +4,14 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.MomentOfInertia
+import edu.wpi.first.units.measure.Voltage
 
-val UNLOCK_VOLTAGE = Units.Volts.of(0.0)
+val UNLOCK_VOLTAGE: Voltage = Units.Volts.of(0.0)
 val OPEN_LATCH_POSITION: Distance = Units.Millimeters.of(0.8)
 val CLOSE_LATCH_POSITION: Distance = Units.Millimeters.of(0.2)
 val LATCH_TOLERANCE: Distance = Units.Millimeters.of(1.0)
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
 val FOLDED_ANGLE: Angle = Units.Degree.of(30.0)
-val FOLDED_TOLERANCE = Units.Degree.of(1.0)
+val FOLDED_TOLERANCE: Angle = Units.Degree.of(1.0)
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.0)
-val DISTANCE_THRESHOLD = Units.Centimeter.of(0.4)
+val DISTANCE_THRESHOLD: Distance = Units.Centimeter.of(0.4)
