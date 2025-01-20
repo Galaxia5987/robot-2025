25486fad09ee929a075111c7b2d78d1530147365
Emma Livne <emmalivne.03@gmail.com>
2025-01-20 21:31:16 +0200

    Replace IO stopper functions with setStopperPower

--- src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climber/ClimberConstants.kt
@@ -9,6 +9,8 @@ import edu.wpi.first.units.measure.MomentOfInertia
 val OPEN_LATCH_POSITION: Distance = Units.Millimeters.of(0.8)
 val CLOSE_LATCH_POSITION: Distance = Units.Millimeters.of(0.2)
 val LATCH_TOLERANCE: Distance = Units.Millimeters.of(1.0)
+val LOCK_POWER = 0.0
+val UNLOCK_POWER = 0.0
 val UNFOLDED_ANGLE: Angle = Units.Degree.of(60.0)
 val FOLDED_ANGLE: Angle = Units.Degree.of(30.0)
 val FOLDED_TOLERANCE: Angle = Units.Degree.of(1.0)
