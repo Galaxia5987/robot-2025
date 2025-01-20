96cd6caf5edc75669510493c7657611e2e4223d3
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-20 10:54:08 +0200

    Change Roller `GEAR_RATIO` to a non zero value

--- src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
@@ -6,5 +6,5 @@ import edu.wpi.first.units.measure.MomentOfInertia
 const val INTAKE_POWER = 0.0
 const val OUTTAKE_POWER = 0.0
 const val FAR_OUTTAKE_POWER = 0.0
-const val GEAR_RATIO = 0.0
+const val GEAR_RATIO = 1.0
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.003)
