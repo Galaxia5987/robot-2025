e0ce79d0e8fde634af4a7191190e2bb2bd6cc441
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-21 20:41:18 +0200

    Fix intake pose

--- src/main/kotlin/frc/robot/subsystems/Visualizer.kt
+++ src/main/kotlin/frc/robot/subsystems/Visualizer.kt
@@ -11,7 +11,7 @@ import frc.robot.lib.getRotation3d
 import frc.robot.lib.getTranslation3d
 import org.littletonrobotics.junction.AutoLogOutput
 
-private val INITIAL_INTAKE_TRANSLATION = getTranslation3d(x = Meters.of(0.47), z = Meters.of(0.35))
+private val INITIAL_INTAKE_TRANSLATION = getTranslation3d(x = Meters.of(-0.25), z = Meters.of(0.35))
 
 private val INITIAL_WRIST_TRANSLATION = getTranslation3d(z = Meters.of(0.59))
 
