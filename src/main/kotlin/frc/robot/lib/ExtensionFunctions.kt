0dcec43123418849ef0d3bee945476e2cdd6b685
Emma Livne <emmalivne.03@gmail.com>
2025-01-21 12:49:47 +0200

    Add extension functions instead of GeomUtil

--- src/main/kotlin/frc/robot/lib/ExtensionFunctions.kt
+++ src/main/kotlin/frc/robot/lib/ExtensionFunctions.kt
@@ -2,7 +2,10 @@ package frc.robot.lib
 
 import com.pathplanner.lib.util.FlippingUtil
 import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Pose3d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.geometry.Transform2d
+import edu.wpi.first.math.geometry.Transform3d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.units.Units
@@ -11,9 +14,9 @@ import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.WrapperCommand
 import frc.robot.IS_RED
+import org.littletonrobotics.junction.LogTable
 import kotlin.math.PI
 import kotlin.math.hypot
-import org.littletonrobotics.junction.LogTable
 
 fun ChassisSpeeds.getSpeed() = hypot(vxMetersPerSecond, vyMetersPerSecond)
 
@@ -83,18 +86,39 @@ fun Command.finallyDo(command: Command): WrapperCommand =
         }
     )
 
+//Geometry extension functions:
 fun Pose2d.flip(): Pose2d = FlippingUtil.flipFieldPose(this)
 
 fun Pose2d.flipIfNeeded(): Pose2d = if (IS_RED) this.flip() else this
+
+fun Pose2d.withTranslation(translation: Translation2d): Pose2d = Pose2d(translation, this.rotation)
+
+fun Pose2d.withRotation(rotation: Rotation2d): Pose2d = Pose2d(this.translation, rotation)
+
+fun Pose2d.toTransform(): Transform2d = Transform2d(this.translation, this.rotation)
+
+fun Pose3d.toTransform(): Transform3d = Transform3d(this.translation, this.rotation)
 
 fun Translation2d.flip(): Translation2d = FlippingUtil.flipFieldPosition(this)
 
 fun Translation2d.flipIfNeeded(): Translation2d =
     if (IS_RED) this.flip() else this
 
+fun Translation2d.toTransform(): Transform2d = Transform2d(this, Rotation2d())
+
+fun Translation2d.toPose(): Pose2d = Pose2d(this, Rotation2d())
+
 fun Rotation2d.flip(): Rotation2d = FlippingUtil.flipFieldRotation(this)
 
 fun Rotation2d.flipIfNeeded(): Rotation2d = if (IS_RED) this.flip() else this
+
+fun Rotation2d.toTransform(): Transform2d = Transform2d(Translation2d(), this)
+
+fun Rotation2d.toPose(): Pose2d = Pose2d(Translation2d(), this)
+
+fun Transform2d.toPose(): Pose2d = Pose2d(this.translation, this.rotation)
+
+fun Transform3d.toPose(): Pose3d = Pose3d(this.translation, this.rotation)
 
 fun Distance.toAngle(radius: Distance, gearRatio: Double): Angle =
     this.timesConversionFactor(
