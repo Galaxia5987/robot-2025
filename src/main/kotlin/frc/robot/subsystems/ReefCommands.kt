bc32e75e57329f8cc5564ef2bc3408c7e5ef1c2a
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-21 20:25:00 +0200

    Add endTriggers

--- src/main/kotlin/frc/robot/subsystems/ReefCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/ReefCommands.kt
@@ -2,24 +2,27 @@ package frc.robot.subsystems
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.Commands.*
+import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.subsystems.elevator.Elevator
 import frc.robot.subsystems.gripper.Gripper
 import frc.robot.subsystems.wrist.Wrist
 
 class ReefCommands(private val elevator: Elevator, private val gripper: Gripper, private val wrist: Wrist) {
+    private fun scoreCoral(endTrigger: Trigger): Command = waitUntil(endTrigger).andThen(gripper.outtake())
 
-    // `.onFalse` will be `gripper.outtake`
-    fun moveL1(): Command = Commands.parallel(elevator.l1(), wrist.l1())
-    fun moveL2(): Command = Commands.parallel(elevator.l2(), wrist.l2())
-    fun moveL3(): Command = Commands.parallel(elevator.l3(), wrist.l3())
-    fun moveL4(): Command = Commands.parallel(elevator.l4(), wrist.l4())
+    fun moveL1(endTrigger: Trigger): Command = parallel(elevator.l1(), wrist.l1()).andThen(scoreCoral(endTrigger))
+    fun moveL2(endTrigger: Trigger): Command = parallel(elevator.l2(), wrist.l2()).andThen(scoreCoral(endTrigger))
+    fun moveL3(endTrigger: Trigger): Command = parallel(elevator.l3(), wrist.l3()).andThen(scoreCoral(endTrigger))
+    fun moveL4(endTrigger: Trigger): Command = parallel(elevator.l4(), wrist.l4()).andThen(scoreCoral(endTrigger))
 
-    fun moveL2algae(): Command = Commands.parallel(elevator.l2Algae(), wrist.l2algae(), gripper.removeAlgae())
-    fun moveL3algae(): Command = Commands.parallel(elevator.l3Algae(), wrist.l3algae(), gripper.removeAlgae())
+    fun moveL3algae(): Command = parallel(elevator.l3Algae(), wrist.l3algae(), gripper.removeAlgae())
+    fun moveL2algae(): Command = parallel(elevator.l2Algae(), wrist.l2algae(), gripper.removeAlgae())
 
-    // `.onFalse` will be `gripper.intake()`
-    fun moveFeeder(): Command = Commands.parallel(elevator.feeder(), wrist.feeder())
+    private fun intakeCoral(endTrigger: Trigger): Command = waitUntil(endTrigger).andThen(gripper.intake())
 
-    fun retract(): Command = Commands.parallel(elevator.zero(), wrist.retract())
+    fun moveFeeder(endTrigger: Trigger): Command = parallel(elevator.feeder(), wrist.feeder()).andThen(intakeCoral(endTrigger))
+
+    fun retract(): Command = parallel(elevator.zero(), wrist.retract())
 
 }
