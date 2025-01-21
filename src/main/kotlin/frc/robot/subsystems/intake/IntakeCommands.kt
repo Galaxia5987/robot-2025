7e4fae33a6b9bf046c7a3956edfb8b6ca41e3c3c
Emma Livne <emmalivne.03@gmail.com>
2025-01-21 15:26:32 +0200

    Apply spotless

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -5,16 +5,22 @@ import edu.wpi.first.wpilibj2.command.Commands
 import frc.robot.subsystems.intake.extender.Extender
 import frc.robot.subsystems.intake.roller.Roller
 
-class IntakeCommands(private val extender: Extender, private val roller: Roller) {
-    fun intake(): Command =
-        extender.extend().alongWith(roller.intake())
+class IntakeCommands(
+    private val extender: Extender,
+    private val roller: Roller
+) {
+    fun intake(): Command = extender.extend().alongWith(roller.intake())
 
     fun outtake(): Command =
         Commands.startEnd(roller::outtake, extender::retract, roller, extender)
 
     fun farOuttake(): Command =
-        Commands.startEnd(roller::farOuttake, extender::retract, roller, extender)
+        Commands.startEnd(
+            roller::farOuttake,
+            extender::retract,
+            roller,
+            extender
+        )
 
-    fun retract(): Command =
-        extender.retract()
+    fun retract(): Command = extender.retract()
 }
