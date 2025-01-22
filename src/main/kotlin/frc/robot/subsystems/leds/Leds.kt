bee39af9a5b343271e50504b648f12423e29a626
yahali david mor <mor.yahali1612@gmail.com>
2025-01-22 22:06:22 +0200

    Spotless

--- src/main/kotlin/frc/robot/subsystems/leds/Leds.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/Leds.kt
@@ -7,8 +7,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 class Leds(private val io: LedIO) : SubsystemBase() {
 
     private fun rainBow(): Command = run { io.setRainbow(SCROLLING_SPEED) }
-    private fun solid(color: LEDPattern): Command = run { io.setLedStrip(color) }
-    private fun blink(color: LEDPattern): Command = run { io.setLedStripBlink(color, BLINKING_TIME) }
+    private fun solid(color: LEDPattern): Command = run {
+        io.setLedStrip(color)
+    }
+    private fun blink(color: LEDPattern): Command = run {
+        io.setLedStripBlink(color, BLINKING_TIME)
+    }
 
     fun intakeLed(): Command = blink(INTAKE_COLOR)
     fun climbLed(): Command = rainBow()
