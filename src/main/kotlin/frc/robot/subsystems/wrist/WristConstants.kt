3f707272438f8b1e65110aeab0637100f6c60c75
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-22 20:30:56 +0200

    Adjust L4 angle

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -12,7 +12,7 @@ enum class Angles(val angle: Angle) {
     L1(Units.Degrees.of(10.0)),
     L2(Units.Degrees.of(35.0)),
     L3(Units.Degrees.of(35.0)),
-    L4(Units.Degrees.of(40.0)),
+    L4(Units.Degrees.of(30.0)),
     L2_ALGAE(Units.Degrees.of(110.0)),
     L3_ALGAE(Units.Degrees.of(110.0)),
     FEEDER(Units.Degrees.of(55.0)),
