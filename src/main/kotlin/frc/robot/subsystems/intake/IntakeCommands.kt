136c7eda5a39059179f742e2dbaba8569e6835e1
Emma Livne <emmalivne.03@gmail.com>
2025-01-22 17:56:32 +0200

    Add endTrigger as parameter for intake commands

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -1,13 +1,17 @@
 package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.extender
 import frc.robot.roller
 
 fun intake(): Command = extender.extend().alongWith(roller.intake())
 
-fun outtake(): Command = roller.outtake()
+fun outtake(endTrigger: Trigger): Command =
+    Commands.parallel(roller.outtake(), Commands.waitUntil(endTrigger).andThen(retract()))
 
-fun farOuttake(): Command = roller.farOuttake()
+fun farOuttake(endTrigger: Trigger): Command =
+    Commands.parallel(roller.farOuttake(), Commands.waitUntil(endTrigger).andThen(retract()))
 
 fun retract(): Command = extender.retract()
