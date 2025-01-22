dd859061d544384f37e0397d2cf978442ae07840
Emma Livne <emmalivne.03@gmail.com>
2025-01-22 16:27:56 +0200

    Move commands out of the class

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -4,12 +4,10 @@ import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.extender
 import frc.robot.roller
 
-class IntakeCommands() {
-    fun intake(): Command = extender.extend().alongWith(roller.intake())
+fun intake(): Command = extender.extend().alongWith(roller.intake())
 
-    fun outtake(): Command = roller.outtake()
+fun outtake(): Command = roller.outtake()
 
-    fun farOuttake(): Command = roller.farOuttake()
+fun farOuttake(): Command = roller.farOuttake()
 
-    fun retract(): Command = extender.retract()
-}
+fun retract(): Command = extender.retract()
