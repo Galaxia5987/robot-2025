e533e407a376554e9792e24faec4acb27cb82370
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 08:00:11 +0200

    Change Trigger to BooleanSupplier

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -3,19 +3,19 @@ package frc.robot.subsystems.intake
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.Commands.waitUntil
-import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.extender
 import frc.robot.roller
+import java.util.function.BooleanSupplier
 
 fun algaeIntake(): Command = extender.extend().alongWith(roller.intake())
 
-fun algaeOuttake(endTrigger: Trigger): Command =
+fun algaeOuttake(endTrigger: BooleanSupplier): Command =
     Commands.parallel(
         roller.outtake(),
         waitUntil(endTrigger).andThen(retractIntake())
     )
 
-fun algaeFarOuttake(endTrigger: Trigger): Command =
+fun algaeFarOuttake(endTrigger: BooleanSupplier): Command =
     Commands.parallel(
         roller.farOuttake(),
         waitUntil(endTrigger).andThen(retractIntake())
