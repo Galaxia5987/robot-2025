90b0e68daa8464a1bd622a9be08f8c0947c7ff21
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 17:54:53 +0200

    Use andThen

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands.sequence
 import frc.robot.extender
 import frc.robot.roller
 import java.util.function.BooleanSupplier
@@ -9,12 +8,9 @@ import java.util.function.BooleanSupplier
 fun intakeAlgae(): Command = extender.extend().alongWith(roller.intake())
 
 fun outtakeAlgae(retractTrigger: BooleanSupplier): Command =
-    sequence(roller.outtake().until(retractTrigger), retractIntake())
+    roller.outtake().until(retractTrigger).andThen(retractIntake())
 
 fun farOuttakeAlgae(retractTrigger: BooleanSupplier): Command =
-    sequence(
-        roller.farOuttake().until(retractTrigger),
-        retractIntake()
-    )
+    roller.farOuttake().until(retractTrigger).andThen(retractIntake())
 
 fun retractIntake(): Command = extender.retract()
