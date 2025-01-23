acfe1cde8d827738733ee7910156537c23a33f40
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-23 17:15:22 +0200

    Add voltage tuning in intake Roller

--- src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
@@ -5,22 +5,29 @@ import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.littletonrobotics.junction.Logger
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber
 
 class Roller(private val io: RollerIO) : SubsystemBase() {
+    private val tuningVoltage =
+        LoggedNetworkNumber("Tuning/Roller/Voltage", 0.0)
 
     private fun setVoltage(voltage: Voltage): Command =
         startEnd(
                 { io.setVoltage(voltage) },
                 { io.setVoltage(Units.Volts.zero()) }
             )
-            .withName("roller/setVoltage")
+            .withName("Roller/setVoltage")
 
-    fun intake() = setVoltage(INTAKE_VOLTAGE).withName("roller/intake")
+    fun setTuningVoltage(): Command =
+        setVoltage(Units.Volts.of(tuningVoltage.get()))
+            .withName("Roller/Voltage")
 
-    fun outtake() = setVoltage(OUTTAKE_VOLTAGE).withName("roller/outtake")
+    fun intake() = setVoltage(INTAKE_VOLTAGE).withName("Roller/intake")
+
+    fun outtake() = setVoltage(OUTTAKE_VOLTAGE).withName("Roller/outtake")
 
     fun farOuttake() =
-        setVoltage(FAR_OUTTAKE_VOLTAGE).withName("roller/farOuttake")
+        setVoltage(FAR_OUTTAKE_VOLTAGE).withName("Roller/farOuttake")
 
     override fun periodic() {
         io.updateInputs()
