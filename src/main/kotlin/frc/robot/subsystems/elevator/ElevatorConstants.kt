6d6246fa6a3a3aa35f996eb8c7395c4c8fd32e1c
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-23 16:23:04 +0200

    Add tunable elevator height

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -11,7 +11,6 @@ import frc.robot.lib.selectGainsBasedOnMode
 import kotlin.math.PI
 
 val MAX_HEIGHT: Distance = Units.Meters.of(1.3)
-val MIN_HEIGHT: Distance = Units.Meters.of(0.0)
 const val GEAR_RATIO = (1.0 / 12.0) * (42.0 / 48.0)
 const val FIRST_STAGE_RATIO = 2.0
 const val ENCODER_OFFSET = 0.0
@@ -30,15 +29,16 @@ val CENTIMETERS_TO_ROTATIONS: Measure<out PerUnit<AngleUnit, DistanceUnit>> =
 val GAINS =
     selectGainsBasedOnMode(Gains(kP = 20.0, kD = 1.0, kG = 1.0), Gains())
 
-enum class Positions(val value: Distance) {
-    L1(Units.Centimeters.of(15.0)),
-    L2(Units.Centimeters.of(32.0)),
-    L3(Units.Centimeters.of(52.0)),
-    L4(Units.Centimeters.of(130.0)),
+enum class Positions(var value: Distance) {
+    L1(Units.Centimeters.of(25.0)),
+    L2(Units.Centimeters.of(35.0)),
+    L3(Units.Centimeters.of(45.0)),
+    L4(Units.Centimeters.of(110.0)),
     L2_ALGAE(Units.Centimeters.of(55.0)),
     L3_ALGAE(Units.Centimeters.of(79.0)),
     FEEDER(Units.Centimeters.of(25.0)),
-    ZERO(Units.Centimeters.zero());
+    ZERO(Units.Centimeters.zero()),
+    TUNING(Units.Centimeters.of(37.6));
 
     fun getLoggingName() =
         name.split("_").joinToString(" ") {
