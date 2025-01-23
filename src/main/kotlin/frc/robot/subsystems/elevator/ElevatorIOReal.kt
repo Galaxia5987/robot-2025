c7a863dd92ac5face9225f0f42c93f89864788c2
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 09:27:10 +0200

    Configure elevator limit switch

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOReal.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.elevator
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs
 import com.ctre.phoenix6.configs.FeedbackConfigs
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
@@ -10,12 +11,15 @@ import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue
+import com.ctre.phoenix6.signals.ReverseLimitTypeValue
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.toAngle
 import frc.robot.lib.toDistance
+import frc.robot.subsystems.intake.extender.EXTENDER_MOTOR_ID
 import org.littletonrobotics.junction.AutoLogOutput
 
 class ElevatorIOReal : ElevatorIO {
@@ -47,6 +51,12 @@ class ElevatorIOReal : ElevatorIO {
                     }
                 Feedback = FeedbackConfigs().apply { RotorToSensorRatio = 1.0 }
                 Slot0 = slot0Configs
+                HardwareLimitSwitch = HardwareLimitSwitchConfigs().apply {
+                    ReverseLimitType = ReverseLimitTypeValue.NormallyOpen
+                    ReverseLimitEnable = true
+                    ReverseLimitSource = ReverseLimitSourceValue.RemoteTalonFX
+                    ReverseLimitRemoteSensorID = EXTENDER_MOTOR_ID
+                }
                 CurrentLimits =
                     CurrentLimitsConfigs().apply {
                         StatorCurrentLimitEnable = true
