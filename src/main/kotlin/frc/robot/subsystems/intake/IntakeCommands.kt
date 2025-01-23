36f419c6708f83a7c0e59016251ea33430ace367
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 17:52:08 +0200

    Use sequence directly

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.Commands.sequence
 import frc.robot.extender
 import frc.robot.roller
 import java.util.function.BooleanSupplier
@@ -9,10 +9,10 @@ import java.util.function.BooleanSupplier
 fun intakeAlgae(): Command = extender.extend().alongWith(roller.intake())
 
 fun outtakeAlgae(retractTrigger: BooleanSupplier): Command =
-    Commands.sequence(roller.outtake().until(retractTrigger), retractIntake())
+    sequence(roller.outtake().until(retractTrigger), retractIntake())
 
 fun farOuttakeAlgae(retractTrigger: BooleanSupplier): Command =
-    Commands.sequence(
+    sequence(
         roller.farOuttake().until(retractTrigger),
         retractIntake()
     )
