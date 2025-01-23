5479c56917fd546ce45055b623e2f953231f3698
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 21:02:35 +0200

    Add motor current inputs

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIO.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIO.kt
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.elevator
 
 import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Current
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
 import org.team9432.annotation.Logged
@@ -20,6 +21,8 @@ interface ElevatorIO {
     open class ElevatorInputs {
         var height: Distance = Units.Meters.zero()
         var appliedVoltage: Voltage = Units.Volts.zero()
+        var mainMotorCurrent: Current = Units.Amps.zero()
+        var auxMotorCurrent: Current = Units.Amps.zero()
         var limitSwitchValue = false
     }
 }
