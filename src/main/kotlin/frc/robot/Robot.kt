5844baa6e54429d7f89600756784746a443d38ac
Dan Katzuv <31829093+katzuv@users.noreply.github.com>
2025-01-24 00:42:18 +0200

    Remove unused pneumatics-related imports

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -6,9 +6,7 @@ package frc.robot
 import edu.wpi.first.hal.FRCNetComm.tInstances
 import edu.wpi.first.hal.FRCNetComm.tResourceType
 import edu.wpi.first.hal.HAL
-import edu.wpi.first.wpilibj.Compressor
 import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.PowerDistribution
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
