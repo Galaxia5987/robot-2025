bfa9efc983cc24d8a78d9ae45d3f0c5e21e76f99
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 21:05:23 +0200

    Change setPower to setVoltage

--- src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/Elevator.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.elevator
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import java.util.function.DoubleSupplier
@@ -40,8 +41,8 @@ class Elevator(private val io: ElevatorIO) : SubsystemBase() {
     fun feeder(): Command = setPosition(Positions.FEEDER)
     fun zero(): Command = setPosition(Positions.ZERO)
 
-    fun setPower(percentOutput: DoubleSupplier): Command = run {
-        io.setPower(percentOutput.asDouble)
+    fun setVoltage(voltage: Voltage): Command = run {
+        io.setVoltage(voltage)
     }
 
     fun reset(): Command = runOnce(io::reset)
