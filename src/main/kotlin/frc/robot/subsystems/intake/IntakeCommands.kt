2eec2c6ce0b2b818348165a8350a3665a0035105
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 07:58:12 +0200

    Use waitUntil instead of Commands.waitUntil

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.Commands.waitUntil
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.extender
 import frc.robot.roller
@@ -11,13 +12,13 @@ fun algaeIntake(): Command = extender.extend().alongWith(roller.intake())
 fun algaeOuttake(endTrigger: Trigger): Command =
     Commands.parallel(
         roller.outtake(),
-        Commands.waitUntil(endTrigger).andThen(retractIntake())
+        waitUntil(endTrigger).andThen(retractIntake())
     )
 
 fun algaeFarOuttake(endTrigger: Trigger): Command =
     Commands.parallel(
         roller.farOuttake(),
-        Commands.waitUntil(endTrigger).andThen(retractIntake())
+        waitUntil(endTrigger).andThen(retractIntake())
     )
 
 fun retractIntake(): Command = extender.retract()
