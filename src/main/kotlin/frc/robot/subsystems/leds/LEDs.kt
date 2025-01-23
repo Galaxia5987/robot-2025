5368b1d105d8c8800ef7daa88a2a80b2767f47db
yahali david mor <mor.yahali1612@gmail.com>
2025-01-23 19:39:35 +0200

    Rebased Leds io real to leds class

--- src/main/kotlin/frc/robot/subsystems/leds/LEDs.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LEDs.kt
@@ -1,21 +1,49 @@
 package frc.robot.subsystems.leds
 
+import edu.wpi.first.wpilibj.AddressableLED
+import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.AddressableLEDBufferView
 import edu.wpi.first.wpilibj.LEDPattern
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class LEDs(private val io: LEDsIO) : SubsystemBase() {
+class LEDs : SubsystemBase() {
+    val ledStrip =
+        AddressableLED(LED_STRIP_PORT).apply { setLength(STRIP_LENGTH) }
+    val ledBuffer = AddressableLEDBuffer(STRIP_LENGTH)
 
-    private fun solid(color: LEDPattern): Command = runOnce {
-        io.setColor(color)
+    init {
+        ledStrip.start()
     }
 
-    private fun blink(color: LEDPattern): Command = runOnce {
-        io.blink(color, BLINKING_TIME)
+    private fun setColor(color: LEDPattern) {
+        color.applyTo(ledBuffer)
     }
-    private fun setLedPattern(pattern: LEDPattern, section: Array<Int>) {
-        io.setPattern(pattern, section)
+
+    private fun blink(primary: LEDPattern) {
+        (primary.blink(BLINKING_TIME)).applyTo(ledBuffer)
     }
+
+    private fun setPattern(
+        primaryPattern: LEDPattern,
+        secondaryPattern: LEDPattern,
+        section: Array<Int>
+    ) {
+        val sectionOfBuffer: AddressableLEDBufferView =
+            ledBuffer.createView(section[0], section[1])
+        secondaryPattern.applyTo(ledBuffer)
+        primaryPattern.applyTo(sectionOfBuffer)
+        ledStrip.setData(ledBuffer)
+    }
+
+    fun intakeLED(): Command = run { blink(INTAKE_COLOR) }
+    fun climbLED(): Command = run {
+        RAINBOW.scrollAtAbsoluteSpeed(SCROLLING_SPEED, LED_SPACING)
+            .applyTo(ledBuffer)
+    }
+
+    fun pattern(): Command = run { setPattern(RED, BLUE, arrayOf(5, 15)) }
+    fun clearLED(): Command = run { TRANSPARENT.applyTo(ledBuffer) }
 
     override fun periodic() {
         ledStrip.setData(ledBuffer)
