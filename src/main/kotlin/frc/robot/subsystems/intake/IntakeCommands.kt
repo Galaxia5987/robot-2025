1f92be8662719da2001c5bacc4130946330a9ddf
Emma Livne <emmalivne.03@gmail.com>
2025-01-23 17:42:03 +0200

    Fix intake commands

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeCommands.kt
@@ -2,7 +2,6 @@ package frc.robot.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.Commands.waitUntil
 import frc.robot.extender
 import frc.robot.roller
 import java.util.function.BooleanSupplier
@@ -10,15 +9,15 @@ import java.util.function.BooleanSupplier
 fun intakeAlgae(): Command = extender.extend().alongWith(roller.intake())
 
 fun outtakeAlgae(retractTrigger: BooleanSupplier): Command =
-    Commands.parallel(
-        roller.outtake(),
-        waitUntil(retractTrigger).andThen(retractIntake())
+    Commands.sequence(
+        roller.outtake().until(retractTrigger),
+        retractIntake()
     )
 
 fun farOuttakeAlgae(retractTrigger: BooleanSupplier): Command =
-    Commands.parallel(
-        roller.farOuttake(),
-        waitUntil(retractTrigger).andThen(retractIntake())
+    Commands.sequence(
+        roller.farOuttake().until(retractTrigger),
+        retractIntake()
     )
 
 fun retractIntake(): Command = extender.retract()
