20dff1fb2d2ea9a59624a36eb9e2c24da3e78f69
yahali david mor <mor.yahali1612@gmail.com>
2025-01-25 17:13:16 +0200

    Fixing rebasing Shit

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -5,13 +5,17 @@ import edu.wpi.first.units.DistanceUnit
 import edu.wpi.first.units.Measure
 import edu.wpi.first.units.PerUnit
 import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Current
 import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.units.measure.Voltage
 import frc.robot.lib.Gains
 import frc.robot.lib.selectGainsBasedOnMode
 import kotlin.math.PI
 
 val MAX_HEIGHT: Distance = Units.Meters.of(1.3)
 val MIN_HEIGHT: Distance = Units.Meters.of(0.0)
+val RESET_VOLTAGE: Voltage = Units.Volts.of(0.0)
+val RESET_CURRENT_THRESHOLD: Current = Units.Amps.of(0.0)
 const val GEAR_RATIO = (1.0 / 12.0) * (42.0 / 48.0)
 const val FIRST_STAGE_RATIO = 2.0
 const val ENCODER_OFFSET = 0.0
@@ -30,16 +34,15 @@ val CENTIMETERS_TO_ROTATIONS: Measure<out PerUnit<AngleUnit, DistanceUnit>> =
 val GAINS =
     selectGainsBasedOnMode(Gains(kP = 20.0, kD = 1.0, kG = 1.0), Gains())
 
-enum class Positions(var value: Distance) {
-    L1(Units.Centimeters.of(25.0)),
-    L2(Units.Centimeters.of(35.0)),
-    L3(Units.Centimeters.of(45.0)),
-    L4(Units.Centimeters.of(110.0)),
+enum class Positions(val value: Distance) {
+    L1(Units.Centimeters.of(15.0)),
+    L2(Units.Centimeters.of(32.0)),
+    L3(Units.Centimeters.of(52.0)),
+    L4(Units.Centimeters.of(130.0)),
     L2_ALGAE(Units.Centimeters.of(55.0)),
     L3_ALGAE(Units.Centimeters.of(79.0)),
     FEEDER(Units.Centimeters.of(25.0)),
-    ZERO(Units.Centimeters.zero()),
-    TUNING(Units.Centimeters.of(37.6));
+    ZERO(Units.Centimeters.zero());
 
     fun getLoggingName() =
         name.split("_").joinToString(" ") {
