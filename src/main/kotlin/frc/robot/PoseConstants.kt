a6c9ba7f056ff37485fac36381c9446cc75a6c77
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-26 13:59:09 +0200

    Create Pose Constants

---
+++ src/main/kotlin/frc/robot/PoseConstants.kt
@@ -0,0 +1,19 @@
+package frc.robot
+
+import edu.wpi.first.math.geometry.Pose3d
+
+val REEF_ONE_POSE = Pose3d() // TODO: Fill in
+val REEF_TWO_POSE = Pose3d() // TODO: Fill in
+val REEF_THREE_POSE = Pose3d() // TODO: Fill in
+val REEF_FOUR_POSE = Pose3d() // TODO: Fill in
+val REEF_FIVE_POSE = Pose3d() // TODO: Fill in
+val REEF_SIX_POSE = Pose3d() // TODO: Fill in
+val REEF_SEVEN_POSE = Pose3d() // TODO: Fill in
+val REEF_EIGHT_POSE = Pose3d() // TODO: Fill in
+val REEF_NINE_POSE = Pose3d() // TODO: Fill in
+val REEF_TEN_POSE = Pose3d() // TODO: Fill in
+val REEF_ELEVEN_POSE = Pose3d() // TODO: Fill in
+val REEF_TWELVE_POSE = Pose3d() // TODO: Fill in
+
+val LEFT_FEEDER_POSE = Pose3d() // TODO: Fill in
+val RIGHT_FEEDER_POSE = Pose3d() // TODO: Fill in
