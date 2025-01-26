b4a68f59be55e1c24d2836e182bce663e573ff78
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-26 18:32:36 +0200

    Add alignWithBestVisionTarget command

--- src/main/kotlin/frc/robot/subsystems/drive/DriveCommands.java
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveCommands.java
@@ -27,6 +27,8 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.vision.Vision;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.LinkedList;
@@ -46,7 +48,10 @@ public class DriveCommands {
     private static final double WHEEL_RADIUS_RAMP_RATE = 0.05; // Rad/Sec^2
     private static final Rotation2d ANGLE_TOLERANCE = Rotation2d.fromDegrees(0.4);
 
-    private DriveCommands() {}
+    private static final PIDController xController = new PIDController(2.0, 0.0, 0.0);
+
+    private DriveCommands() {
+    }
 
     private static Translation2d getLinearVelocityFromJoysticks(double x, double y) {
         // Apply deadband
@@ -148,7 +153,7 @@ public class DriveCommands {
                                             speeds,
                                             isFlipped
                                                     ? drive.getRotation()
-                                                            .plus(new Rotation2d(Math.PI))
+                                                    .plus(new Rotation2d(Math.PI))
                                                     : drive.getRotation()));
                         },
                         drive)
@@ -224,7 +229,9 @@ public class DriveCommands {
                                 }));
     }
 
-    /** Measures the robot's wheel radius by spinning in a circle. */
+    /**
+     * Measures the robot's wheel radius by spinning in a circle.
+     */
     public static Command wheelRadiusCharacterization(Drive drive) {
         SlewRateLimiter limiter = new SlewRateLimiter(WHEEL_RADIUS_RAMP_RATE);
         WheelRadiusCharacterizationState state = new WheelRadiusCharacterizationState();
@@ -302,10 +309,14 @@ public class DriveCommands {
                                                             + formatter.format(wheelRadius)
                                                             + " meters, "
                                                             + formatter.format(
-                                                                    Units.metersToInches(
-                                                                            wheelRadius))
+                                                            Units.metersToInches(
+                                                                    wheelRadius))
                                                             + " inches");
                                         })));
+    }
+
+    public static Command alignWithBestVisionTarget(Vision vision, int cameraIndex) {
+        return frc.robot.InitializerKt.getSwerveDrive().setDesiredHeading(vision.getTargetX(cameraIndex));
     }
 
     private static class WheelRadiusCharacterizationState {
