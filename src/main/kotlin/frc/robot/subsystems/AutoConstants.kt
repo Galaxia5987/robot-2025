0d09e937e398d7306356484f1ee5e5562a16b1fd
noga <noga.veksler@gmail.com>
2025-01-27 21:09:36 +0200

    Add val SCORE_DISTANCE

--- src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
@@ -4,7 +4,16 @@ import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 
-// TODO: think of better names
-val SCORE_2R_POINT = Translation2d()
+val SCORE_FEEDER_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_1L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_1R_DISTANCE: Distance = Units.Meters.of(0.0)
 val SCORE_2R_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_2L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_3L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_3R_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_4L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_5R_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_5L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_6R_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_6L_DISTANCE: Distance = Units.Meters.of(0.0)
 val AUTO_DISTANCE_TOLERANCE: Distance = Units.Centimeters.of(2.0)
