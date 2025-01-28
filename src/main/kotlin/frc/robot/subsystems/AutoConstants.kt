3d4e2bec18dc80065d1dad237fa9dc73106996a1
Emma Livne <emmalivne.03@gmail.com>
2025-01-28 21:26:45 +0200

    Shorten auto routines file

--- src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
@@ -4,15 +4,5 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 
 val INTAKE_FEEDER_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_1L_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_1R_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_2R_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_2L_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_3L_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_3R_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_4L_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_5R_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_5L_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_6R_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_6L_DISTANCE: Distance = Units.Meters.of(0.0)
+val SCORE_DISTANCE: Distance = Units.Meters.of(0.0)
 val AUTO_DISTANCE_TOLERANCE: Distance = Units.Centimeters.of(2.0)
--- src/main/kotlin/frc/robot/subsystems/AutoRoutines.kt
+++ src/main/kotlin/frc/robot/subsystems/AutoRoutines.kt
@@ -28,6 +28,16 @@ private fun AutoTrajectory.atDistanceFromFinalPoint(distance: Distance): Trigger
     )
 }
 
+private fun AutoTrajectory.scoreAtDistance(distance: Distance = SCORE_DISTANCE, outtakeTrigger: Trigger = this.done()) {
+    this.atDistanceFromFinalPoint(distance)
+        .onTrue(l4(outtakeTrigger))
+}
+
+private fun AutoTrajectory.feedAtDistance(distance: Distance = INTAKE_FEEDER_DISTANCE, intakeTrigger: Trigger = this.done()) {
+    this.atDistanceFromFinalPoint(distance)
+        .onTrue(feeder(intakeTrigger))
+}
+
 private fun leaveRoutine(startingPoint: String): AutoRoutine {
     val routine = autoFactory.newRoutine("$startingPoint Leave")
     val trajectory = routine.trajectory("$startingPoint Leave")
@@ -49,41 +59,29 @@ private fun CLeave(): AutoRoutine =
 
 private fun routine3LR(): AutoRoutine {
     val routine = autoFactory.newRoutine("3LR")
-
     val t3LS = routine.trajectory("3LS")
     val S3R = routine.trajectory("S3R")
 
-    routine.active()
-        .onTrue(t3LS.cmd())
-    t3LS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t3LS.done()))
-
+    routine.active().onTrue(t3LS.cmd())
+    t3LS.feedAtDistance()
     t3LS.done().onTrue(S3R.cmd())
-    S3R.atDistanceFromFinalPoint(SCORE_3R_DISTANCE)
-        .onTrue(l4(S3R.done()))
+    S3R.scoreAtDistance()
 
     return routine
 }
 
 private fun A2R3LR(): AutoRoutine {
     val routine = autoFactory.newRoutine("A2R3LR")
-
     val A2R = routine.trajectory("A2R")
     val t2RS = routine.trajectory("2RS")
     val S3L = routine.trajectory("S3L")
 
     routine.active().onTrue(A2R.cmd())
-    A2R.atDistanceFromFinalPoint(SCORE_2R_DISTANCE)
-        .onTrue(l4(A2R.done()))
-
+    A2R.scoreAtDistance()
     A2R.done().onTrue(t2RS.cmd())
-    t2RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t2RS.done()))
-
+    t2RS.feedAtDistance()
     t2RS.done().onTrue(S3L.cmd())
-    S3L.atDistanceFromFinalPoint(SCORE_3L_DISTANCE)
-        .onTrue(l4(S3L.done()))
-
+    S3L.scoreAtDistance()
     S3L.done().onTrue(routine3LR().cmd())
 
     return routine
@@ -92,134 +90,109 @@ private fun A2R3LR(): AutoRoutine {
 
 private fun A3LR4L(): AutoRoutine {
     val routine = autoFactory.newRoutine("A3LR4L")
-
     val A3L = routine.trajectory("A3L")
     val t3RS = routine.trajectory("3RS")
     val S4L = routine.trajectory("S4L")
 
     routine.active().onTrue(A3L.cmd())
-    A3L.atDistanceFromFinalPoint(SCORE_3L_DISTANCE)
-        .onTrue(l4(A3L.done()))
-
+    A3L.scoreAtDistance()
     A3L.done().onTrue(routine3LR().cmd().andThen(t3RS.cmd()))
-
-    t3RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t3RS.done()))
-
+    t3RS.feedAtDistance()
     t3RS.done().onTrue(S4L.cmd())
-    S4L.atDistanceFromFinalPoint(SCORE_4L_DISTANCE)
-        .onTrue(l4(S4L.done()))
+    S4L.scoreAtDistance()
+
+    return routine
+}
+
+private fun B1L(): AutoRoutine {
+    val routine = autoFactory.newRoutine("B1L")
+    val B1L = routine.trajectory("B1L")
+
+    routine.active().onTrue(B1L.cmd())
+    B1L.scoreAtDistance()
 
     return routine
 }
 
 private fun B1R(): AutoRoutine {
     val routine = autoFactory.newRoutine("B1R")
-
     val B1R = routine.trajectory("B1R")
 
     routine.active().onTrue(B1R.cmd())
-    B1R.atDistanceFromFinalPoint(SCORE_1R_DISTANCE)
-        .onTrue(l4(B1R.done()))
+    B1R.scoreAtDistance()
 
     return routine
 }
 
 private fun B1L6RL(): AutoRoutine {
     val routine = autoFactory.newRoutine("B1L6RL")
-
-    val B1L = routine.trajectory("B1L")
     val t1LS = routine.trajectory("1LS")
     val S6R = routine.trajectory("S6R")
     val t6RS = routine.trajectory("6RS")
     val S6L = routine.trajectory("S6L")
 
-    routine.active().onTrue(B1L.cmd())
-    B1L.atDistanceFromFinalPoint(SCORE_1L_DISTANCE)
-        .onTrue(l4(B1L.done()))
-
-    B1L.done().onTrue(t1LS.cmd())
-    t1LS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t1LS.done()))
-
+    routine.active().onTrue(B1L().cmd().andThen(t1LS.cmd()))
+    t1LS.feedAtDistance()
     t1LS.done().onTrue(S6R.cmd())
-    S6R.atDistanceFromFinalPoint(SCORE_6R_DISTANCE)
-        .onTrue(l4(S6R.done()))
-
+    S6R.scoreAtDistance()
     S6R.done().onTrue(t6RS.cmd())
-    t6RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t6RS.done()))
-
+    t6RS.feedAtDistance()
     t6RS.done().onTrue(S6L.cmd())
-    S6L.atDistanceFromFinalPoint(SCORE_6L_DISTANCE)
-        .onTrue(l4(S6L.done()))
+    S6L.scoreAtDistance()
 
     return routine
 }
 
 private fun B1R2LR(): AutoRoutine {
     val routine = autoFactory.newRoutine("B1R2LR")
-
     val t1RS = routine.trajectory("1RS")
     val S2L = routine.trajectory("S2L")
     val t3LS = routine.trajectory("3LS")
     val S2R = routine.trajectory("S2R")
 
     routine.active().onTrue(B1R().cmd().andThen(t1RS.cmd()))
-
-    t1RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t1RS.done()))
-
+    t1RS.feedAtDistance()
     t1RS.done().onTrue(S2L.cmd())
-    S2L.atDistanceFromFinalPoint(SCORE_2L_DISTANCE)
-        .onTrue(l4(S2L.done()))
-
+    S2L.scoreAtDistance()
     S2L.done().onTrue(t3LS.cmd())
-    t3LS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t3LS.done()))
-
+    t3LS.feedAtDistance()
     t3LS.done().onTrue(S2R.cmd())
-    S2R.atDistanceFromFinalPoint(SCORE_2R_DISTANCE)
-        .onTrue(l4(S2R.done()))
+    S2R.scoreAtDistance()
+
+    return routine
+}
+
+private fun routine5RL(): AutoRoutine {
+    val routine = autoFactory.newRoutine("5RL")
+    val t5RS = routine.trajectory("5RS")
+    val S5L = routine.trajectory("S5L")
+
+    routine.active().onTrue(t5RS.cmd())
+    t5RS.feedAtDistance()
+    t5RS.done().onTrue(S5L.cmd())
+    S5L.scoreAtDistance()
 
     return routine
 }
 
 private fun C5RL4R(): AutoRoutine {
     val routine = autoFactory.newRoutine("C5RL4R")
-
     val C5R = routine.trajectory("C5R")
-    val t5RS = routine.trajectory("5RS")
-    val S5L = routine.trajectory("S5L")
     val t5LS = routine.trajectory("5LS")
     val S4L = routine.trajectory("S4L")
 
     routine.active().onTrue(C5R.cmd())
-    C5R.atDistanceFromFinalPoint(SCORE_5R_DISTANCE)
-        .onTrue(l4(C5R.done()))
-
-    C5R.done().onTrue(t5RS.cmd())
-    t5RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t5RS.done()))
-
-    t5RS.done().onTrue(S5L.cmd())
-    S5L.atDistanceFromFinalPoint(SCORE_5L_DISTANCE)
-        .onTrue(l4(S5L.done()))
-
-    S5L.done().onTrue(t5LS.cmd())
-    t5LS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t5LS.done()))
-
+    C5R.scoreAtDistance()
+    C5R.done().onTrue(routine5RL().cmd().andThen(t5LS.cmd()))
+    t5LS.feedAtDistance()
     t5LS.done().onTrue(S4L.cmd())
-    S4L.atDistanceFromFinalPoint(SCORE_4L_DISTANCE)
-        .onTrue(l4(S4L.done()))
+    S4L.scoreAtDistance()
 
     return routine
 }
 
 private fun C6L5RL(): AutoRoutine {
     val routine = autoFactory.newRoutine("C6L5RL")
-
     val C6L = routine.trajectory("C6L")
     val t6LS = routine.trajectory("6LS")
     val S5R = routine.trajectory("S5R")
@@ -227,27 +200,15 @@ private fun C6L5RL(): AutoRoutine {
     val S5L = routine.trajectory("S5L")
 
     routine.active().onTrue(C6L.cmd())
-    C6L.atDistanceFromFinalPoint(SCORE_6L_DISTANCE)
-        .onTrue(l4(C6L.done()))
-
+    C6L.scoreAtDistance()
     C6L.done().onTrue(t6LS.cmd())
-    t6LS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t6LS.done()))
-
+    t6LS.feedAtDistance()
     t6LS.done().onTrue(S5R.cmd())
-    S5R.atDistanceFromFinalPoint(SCORE_5R_DISTANCE)
-        .onTrue(l4(S5R.done()))
-
-    S5R.done().onTrue(t5RS.cmd())
-    t5RS.atDistanceFromFinalPoint(INTAKE_FEEDER_DISTANCE)
-        .onTrue(feeder(t5RS.done()))
-
-    t5RS.done().onTrue(S5L.cmd())
-    S5L.atDistanceFromFinalPoint(SCORE_5L_DISTANCE)
-        .onTrue(feeder(S5L.done()))
+    S5R.scoreAtDistance()
+    S5R.done().onTrue(routine5RL().cmd())
 
     return routine
 }
 
 val autoRoutines =
-    arrayOf(A2R3LR(), A3LR4L(), ALeave(), BLeave(), CLeave(), B1L6RL(), B1R2LR(), B1R(), C5RL4R(), C6L5RL())
+    arrayOf(ALeave(), BLeave(), CLeave(), B1L(), B1R(), A3LR4L(), A2R3LR(), B1L6RL(), B1R2LR(), C6L5RL(), C5RL4R())
