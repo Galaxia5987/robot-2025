ccbce2d8454c13760d360dce5131df3bd155c5df
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-01-29 15:49:31 +0200

    Add GamePiece logging

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -141,10 +141,14 @@ object Robot : LoggedRobot() {
 
     override fun simulationPeriodic() {
         SimulatedArena.getInstance().simulationPeriodic()
-        Logger.recordOutput("FieldSimulation/Algae",
-            *SimulatedArena.getInstance().getGamePiecesArrayByType("Algae"));
-        Logger.recordOutput("FieldSimulation/Coral",
-            *SimulatedArena.getInstance().getGamePiecesArrayByType("Coral"));
+        Logger.recordOutput(
+            "FieldSimulation/Algae",
+            *SimulatedArena.getInstance().getGamePiecesArrayByType("Algae")
+        )
+        Logger.recordOutput(
+            "FieldSimulation/Coral",
+            *SimulatedArena.getInstance().getGamePiecesArrayByType("Coral")
+        )
     }
 
     /** This function is called periodically during operator control. */
