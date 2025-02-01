34b67399293daa26e880c37efc1263ca3b2e6a68
Emma Livne <emmalivne.03@gmail.com>
2025-02-01 13:48:38 +0200

    Fix controls

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -105,10 +105,8 @@ object RobotContainer {
             .onTrue(feeder(driverController.start().negate()))
         driverController.rightTrigger().onTrue(gripper.intake())
         driverController.leftTrigger().onTrue(gripper.outtake())
-        driverController
-            .rightBumper()
-            .whileTrue(intakeAlgae())
-            .onFalse(outtakeAlgae(driverController.rightBumper().negate()))
+        driverController.rightBumper().whileTrue(intakeAlgae())
+        driverController.leftBumper().onTrue(outtakeAlgae(driverController.leftBumper().negate()))
 
         operatorController.a().onTrue(climber.fold())
         operatorController.b().onTrue(climber.unfold())
