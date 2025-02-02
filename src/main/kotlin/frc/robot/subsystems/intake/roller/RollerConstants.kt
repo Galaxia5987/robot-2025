7aebfe9a5439c1705e2c886b7aa61e274d17453a
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-02-02 16:12:43 +0200

    Add explicit type specifications

--- src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
@@ -1,16 +1,16 @@
 package frc.robot.subsystems.intake.roller
 
 import edu.wpi.first.units.Units
-import edu.wpi.first.units.measure.MomentOfInertia
+import edu.wpi.first.units.measure.*
 
+val CORAL_OUTTAKE_HEIGHT: Distance = Units.Meter.of(0.4)
+val CORAL_OUTTAKE_VELOCITY: LinearVelocity = Units.MetersPerSecond.of(3.0)
+val CORAL_OUTTAKE_ANGLE: Angle = Units.Degrees.zero()
+val INTAKE_WIDTH: Distance = Units.Meters.of(0.64)
+val INTAKE_LENGTH_EXTENDED: Distance = Units.Meters.of(0.35)
 
-val CORAL_OUTTAKE_HEIGHT = Units.Meter.of(0.4)
-val CORAL_OUTTAKE_VELOCITY = Units.MetersPerSecond.of(3.0)
-val CORAL_OUTTAKE_ANGLE = Units.Degrees.zero()
-val INTAKE_WIDTH = Units.Meters.of(0.64)
-val INTAKE_LENGTH_EXTENDED = Units.Meters.of(0.35)
-val INTAKE_VOLTAGE = Units.Volts.of(0.4 * 12.0)
-val OUTTAKE_VOLTAGE = Units.Volts.of(0.5 * 12.0)
-val FAR_OUTTAKE_VOLTAGE = Units.Volts.of(0.7 * 12.0)
+val INTAKE_VOLTAGE: Voltage = Units.Volts.of(0.4 * 12.0)
+val OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.5 * 12.0)
+val FAR_OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.7 * 12.0)
 const val GEAR_RATIO = 1.0
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.003)
