518fb181039379ef221e8f8287f9e15568e8d726
Emma Livne <emmalivne.03@gmail.com>
2025-02-02 22:07:51 +0200

    Calibrate constants

--- src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
@@ -2,9 +2,10 @@ package frc.robot.subsystems.intake.roller
 
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.MomentOfInertia
+import edu.wpi.first.units.measure.Voltage
 
-val INTAKE_VOLTAGE = Units.Volts.of(0.4 * 12.0)
-val OUTTAKE_VOLTAGE = Units.Volts.of(0.5 * 12.0)
-val FAR_OUTTAKE_VOLTAGE = Units.Volts.of(0.7 * 12.0)
+val INTAKE_VOLTAGE: Voltage = Units.Volts.of(-0.5 * 12.0)
+val OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.8 * 12.0)
+val FAR_OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.7 * 12.0)
 const val GEAR_RATIO = 1.0
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.003)
