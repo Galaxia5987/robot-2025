92282c0e82f495d9b9a3d3fd5061086967ef5f46
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-02-03 22:11:10 +0200

    Calibrate `DISTANCE_THRESHOLD`

--- src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/gripper/GripperConstants.kt
@@ -8,7 +8,7 @@ import edu.wpi.first.units.measure.Voltage
 
 const val GEAR_RATIO = 1.0 // TODO: Replace with real value
 
-val DISTANCE_THRESHOLD: Distance = Units.Meter.of(0.19)
+val DISTANCE_THRESHOLD: Distance = Units.Centimeters.of(14.2)
 val INTAKE_VOLTAGE: Voltage = Units.Volts.of(0.3) // TODO: Calibrate
 val OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(-0.3) // TODO: Calibrate
 val REMOVE_ALGAE_VOLTAGE: Voltage = Units.Volts.of(0.3) // TODO: Calibrate
