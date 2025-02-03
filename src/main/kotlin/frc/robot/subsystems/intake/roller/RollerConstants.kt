a77a2efc893eb29b2e29be95bc53652119c8bf72
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-02-03 21:38:19 +0200

    Increase INTAKE_VOLTAGE

--- src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
@@ -4,7 +4,7 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.MomentOfInertia
 import edu.wpi.first.units.measure.Voltage
 
-val INTAKE_VOLTAGE: Voltage = Units.Volts.of(-0.5 * 12.0)
+val INTAKE_VOLTAGE: Voltage = Units.Volts.of(-0.7 * 12.0)
 val OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.8 * 12.0)
 val FAR_OUTTAKE_VOLTAGE: Voltage = Units.Volts.of(0.7 * 12.0)
 const val GEAR_RATIO = 1.0
