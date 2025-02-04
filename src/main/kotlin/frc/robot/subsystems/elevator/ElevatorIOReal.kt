d606d501abee41d8a594f8f3ff7fd2698d553dee
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-02-04 19:42:40 +0200

    Switch to PositionVoltage + Correct CANBUS

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorIOReal.kt
@@ -3,12 +3,14 @@ package frc.robot.subsystems.elevator
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs
 import com.ctre.phoenix6.configs.FeedbackConfigs
 import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs
+import com.ctre.phoenix6.configs.MotionMagicConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.Follower
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC
+import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.InvertedValue
@@ -21,6 +23,7 @@ import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.REEFMASTER_CANBUS_NAME
 import frc.robot.lib.toAngle
 import frc.robot.lib.toDistance
 import frc.robot.subsystems.intake.extender.MOTOR_ID as EXTENDER_MOTOR_ID
@@ -29,9 +32,9 @@ import org.littletonrobotics.junction.AutoLogOutput
 
 class ElevatorIOReal : ElevatorIO {
     override val inputs = LoggedElevatorInputs()
-    private val mainMotor = TalonFX(MAIN_ID)
-    private val auxMotor = TalonFX(AUX_ID)
-    private val mainMotorPositionRequest = MotionMagicTorqueCurrentFOC(0.0)
+    private val mainMotor = TalonFX(MAIN_ID, REEFMASTER_CANBUS_NAME)
+    private val auxMotor = TalonFX(AUX_ID, REEFMASTER_CANBUS_NAME)
+    private val mainMotorPositionRequest = PositionVoltage(0.0)
     private val voltageControl = VoltageOut(0.0)
     private val slot0Configs =
         Slot0Configs().apply {
@@ -64,18 +67,10 @@ class ElevatorIOReal : ElevatorIO {
                 MotorOutput =
                     MotorOutputConfigs().apply {
                         NeutralMode = NeutralModeValue.Brake
-                        Inverted = InvertedValue.Clockwise_Positive
+                        Inverted = InvertedValue.CounterClockwise_Positive
                     }
                 Feedback = FeedbackConfigs().apply { RotorToSensorRatio = 1.0 }
                 Slot0 = slot0Configs
-                HardwareLimitSwitch =
-                    HardwareLimitSwitchConfigs().apply {
-                        ReverseLimitType = ReverseLimitTypeValue.NormallyOpen
-                        ReverseLimitEnable = true
-                        ReverseLimitSource =
-                            ReverseLimitSourceValue.RemoteTalonFX
-                        ReverseLimitRemoteSensorID = EXTENDER_MOTOR_ID
-                    }
                 SoftwareLimitSwitch = softLimitsConfig
                 CurrentLimits =
                     CurrentLimitsConfigs().apply {
