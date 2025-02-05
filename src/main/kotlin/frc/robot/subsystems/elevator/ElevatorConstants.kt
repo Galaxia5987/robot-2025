297e931754dd784c6edc69b8af54541ecdc5273e
Emma Livne <emmalivne.03@gmail.com>
2025-02-05 19:15:50 +0200

    Calibrate l4 position

--- src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/elevator/ElevatorConstants.kt
@@ -38,7 +38,7 @@ enum class Positions(val value: Distance) {
     L1(Units.Centimeters.of(0.0)),
     L2(Units.Centimeters.of(0.0)),
     L3(Units.Centimeters.of(50.0)),
-    L4(Units.Centimeters.of(113.0)),
+    L4(Units.Centimeters.of(100.0)),
     L2_ALGAE(Units.Centimeters.of(55.0)),
     L3_ALGAE(Units.Centimeters.of(79.0)),
     FEEDER(Units.Centimeters.of(25.0)),
