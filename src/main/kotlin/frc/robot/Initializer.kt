1cb5fb67f6391454f7a50ee6edbc3e2caa8ea6c7
yahali david mor <mor.yahali1612@gmail.com>
2025-02-06 19:41:30 +0200

    Add leds Init to Initializer

--- src/main/kotlin/frc/robot/Initializer.kt
+++ src/main/kotlin/frc/robot/Initializer.kt
@@ -1,46 +1,17 @@
 package frc.robot
 
-import frc.robot.subsystems.climber.Climber
-import frc.robot.subsystems.climber.ClimberIO
-import frc.robot.subsystems.climber.ClimberIOReal
-import frc.robot.subsystems.climber.ClimberIOSim
-import frc.robot.subsystems.climber.LoggedClimberInputs
-import frc.robot.subsystems.drive.Drive
-import frc.robot.subsystems.drive.GyroIO
-import frc.robot.subsystems.drive.GyroIONavX
-import frc.robot.subsystems.drive.ModuleIO
-import frc.robot.subsystems.drive.ModuleIOSim
-import frc.robot.subsystems.drive.ModuleIOTalonFX
-import frc.robot.subsystems.drive.TunerConstants
-import frc.robot.subsystems.elevator.Elevator
-import frc.robot.subsystems.elevator.ElevatorIO
-import frc.robot.subsystems.elevator.ElevatorIOReal
-import frc.robot.subsystems.elevator.ElevatorIOSim
-import frc.robot.subsystems.elevator.LoggedElevatorInputs
-import frc.robot.subsystems.gripper.Gripper
-import frc.robot.subsystems.gripper.GripperIO
-import frc.robot.subsystems.gripper.GripperIOReal
-import frc.robot.subsystems.gripper.GripperIOSim
-import frc.robot.subsystems.gripper.LoggedGripperInputs
-import frc.robot.subsystems.intake.extender.Extender
-import frc.robot.subsystems.intake.extender.ExtenderIO
-import frc.robot.subsystems.intake.extender.ExtenderIOReal
-import frc.robot.subsystems.intake.extender.ExtenderIOSim
-import frc.robot.subsystems.intake.extender.LoggedExtenderInputs
-import frc.robot.subsystems.intake.roller.LoggedRollerInputs
-import frc.robot.subsystems.intake.roller.Roller
-import frc.robot.subsystems.intake.roller.RollerIO
-import frc.robot.subsystems.intake.roller.RollerIOReal
-import frc.robot.subsystems.intake.roller.RollerIOSim
+import frc.robot.subsystems.climber.*
+import frc.robot.subsystems.drive.*
+import frc.robot.subsystems.elevator.*
+import frc.robot.subsystems.gripper.*
+import frc.robot.subsystems.intake.extender.*
+import frc.robot.subsystems.intake.roller.*
+import frc.robot.subsystems.leds.LEDs
 import frc.robot.subsystems.vision.Vision
 import frc.robot.subsystems.vision.VisionConstants
 import frc.robot.subsystems.vision.VisionIOPhotonVision
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim
-import frc.robot.subsystems.wrist.LoggedWristInputs
-import frc.robot.subsystems.wrist.Wrist
-import frc.robot.subsystems.wrist.WristIO
-import frc.robot.subsystems.wrist.WristIOReal
-import frc.robot.subsystems.wrist.WristIOSim
+import frc.robot.subsystems.wrist.*
 
 private val swerveModuleIOs =
     arrayOf(
@@ -152,3 +123,5 @@ val wrist =
                 }
         }
     )
+
+val leds = LEDs()
