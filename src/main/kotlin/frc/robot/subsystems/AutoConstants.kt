bc48cb13b58487cc75cb9376b343b60cfc40d296
Emma Livne <emmalivne.03@gmail.com>
2025-02-11 18:13:40 +0200

    Adjust auto constants

--- src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/AutoConstants.kt
@@ -5,7 +5,7 @@ import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Time
 
 val INTAKE_FEEDER_DISTANCE: Distance = Units.Meters.of(0.0)
-val INTAKE_FEEDER_TIME: Time = Units.Seconds.of(0.0)
+val INTAKE_FEEDER_TIME: Time = Units.Seconds.of(3.0)
 val SCORE_DISTANCE: Distance = Units.Meters.of(0.0)
-val SCORE_TIME: Time = Units.Seconds.of(0.0)
+val SCORE_TIME: Time = Units.Seconds.of(5.0)
 val AUTO_DISTANCE_TOLERANCE: Distance = Units.Centimeters.of(2.0)
