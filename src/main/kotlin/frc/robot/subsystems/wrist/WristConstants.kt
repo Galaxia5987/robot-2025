802aba272723923322667935c165f02d707aea60
Emma Livne <emmalivne.03@gmail.com>
2025-02-14 10:48:01 +0200

    Calibrate eevator and wrist

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -13,15 +13,15 @@ const val ROTOR_TO_SENSOR = 1 / ((1.0 / 25.0) * (40.0 / 40.0))
 const val SENSOR_TO_MECHANISM = 1 / (16.0 / 42.0)
 const val GEAR_RATIO = 1 / ((1.0 / 25.0) * (40.0 / 40.0) * (16.0 / 42.0))
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.001)
-val ENCODER_OFFSET: Angle = Units.Radians.of(-5.824525051601779)
+val ENCODER_OFFSET: Angle = Units.Radians.of(-12.138389974539079)
 
 val GAINS = selectGainsBasedOnMode(Gains(kP = 75.0, kG = 0.2), Gains())
 
 enum class Angles(val angle: Angle) {
     L1(Units.Degrees.of(100.0)),
     L2(Units.Degrees.of(80.0)),
-    L3(Units.Degrees.of(80.0)),
-    L4(Units.Degrees.of(75.0)),
+    L3(Units.Degrees.of(95.0)),
+    L4(Units.Degrees.of(87.0)),
     L2_ALGAE(Units.Degrees.of(100.0)),
     L3_ALGAE(Units.Degrees.of(100.0)),
     FEEDER(Units.Degrees.of(25.0)),
