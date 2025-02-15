3d78dacae0e59cbef1b90ec16de7d55200ebbe51
Emma <emmalivne.03@gmail.com>
2025-02-15 02:41:41 +0200

    Flip pose when needed and make parameters non nullable

--- src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
@@ -3,8 +3,9 @@ package frc.robot.autonomous
 import com.pathplanner.lib.auto.AutoBuilder
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.IS_RED
 import frc.robot.subsystems.drive.DriveCommands
-import frc.robot.subsystems.drive.TunerConstants
+import frc.robot.subsystems.drive.TunerConstants.PATH_CONSTRAINTS
 import frc.robot.swerveDrive
 import frc.robot.vision
 import java.util.function.DoubleSupplier
@@ -12,12 +13,15 @@ import java.util.function.DoubleSupplier
 
 fun alignWithBestVisionTarget(
     cameraIndex: Int,
-    ySupplier: DoubleSupplier?,
-    xSupplier: DoubleSupplier?
+    ySupplier: DoubleSupplier,
+    xSupplier: DoubleSupplier
 ): Command =
     DriveCommands.joystickDriveAtAngle(
         swerveDrive, ySupplier, xSupplier
     ) { vision.getYawToTarget(cameraIndex) }
 
-fun alignToPose(pose: Pose2d?): Command =
-    AutoBuilder.pathfindToPose(pose, TunerConstants.PATH_CONSTRAINTS, 0.0)
+fun alignToPose(pose: Pose2d): Command =
+    if (IS_RED)
+        AutoBuilder.pathfindToPoseFlipped(pose, PATH_CONSTRAINTS, 0.0)
+    else
+        AutoBuilder.pathfindToPose(pose, PATH_CONSTRAINTS, 0.0)
