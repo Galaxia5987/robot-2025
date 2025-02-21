4fa6aba2914abf9ff7f70fba6cd7f0b66bd2f27b
Emma Livne <emmalivne.03@gmail.com>
2025-02-21 02:04:23 +0200

    Apply spotless

--- src/main/kotlin/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/kotlin/frc/robot/subsystems/drive/GyroIONavX.java
@@ -18,7 +18,6 @@ import com.studica.frc.AHRS.NavXComType;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
-
 import java.util.Queue;
 
 /** IO implementation for NavX. */
