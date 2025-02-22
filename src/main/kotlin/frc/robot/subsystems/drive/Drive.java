dd4ce2bf6006272e1e982b8211ff26152e7bd1fd
Emma Livne <emmalivne.03@gmail.com>
2025-02-22 21:01:29 +0200

    Calibrate auto builder

--- src/main/kotlin/frc/robot/subsystems/drive/Drive.java
+++ src/main/kotlin/frc/robot/subsystems/drive/Drive.java
@@ -193,7 +193,7 @@ public class Drive extends SubsystemBase {
                 this::getChassisSpeeds,
                 this::runVelocity,
                 new PPHolonomicDriveController(
-                        new PIDConstants(6.0, 0.0, 0.5), new PIDConstants(14.0, 0.0, 0.0)),
+                        new PIDConstants(4.0, 0.0, 2.0), new PIDConstants(14.0, 0.0, 0.0)),
                 PP_CONFIG,
                 () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
                 this);
