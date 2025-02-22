2badf054b64ce8868076a7d390c8a199443b82b9
Emma Livne <emmalivne.03@gmail.com>
2025-02-22 21:10:52 +0200

    Remove auto score and end auto score

--- src/main/kotlin/frc/robot/autonomous/ScoreState.kt
+++ src/main/kotlin/frc/robot/autonomous/ScoreState.kt
@@ -25,29 +25,3 @@ private val selectedHeightPose: () -> ScoreHeight = { ScoreHeight.L3 }
 
 private fun selectedHeightCommand(outtakeTrigger: Trigger): Command =
     selectedHeightPose.invoke().command.invoke(outtakeTrigger)
-
-fun autoScore(): Command =
-    (selectedHeightCommand(Trigger { false })
-            .alongWith(
-                alignToPose(
-                    swerveDrive,
-                    { swerveDrive.pose },
-                    selectedScorePose
-                )
-            ))
-        .onlyIf {
-            swerveDrive.pose.distanceFromPoint(
-                selectedScorePose.invoke().translation
-            ) <= MAX_ALIGNMENT_DISTANCE
-        }
-        .alongWith(Commands.runOnce({ println("AUTO SCORE CALLED!!!") }))
-
-fun endAutoScore(): Command =
-    ConditionalCommand(
-        selectedHeightCommand(Trigger { true }),
-        Commands.runOnce({ swerveDrive.stop() })
-    ) {
-        swerveDrive.pose.distanceFromPoint(
-            selectedScorePose.invoke().translation
-        ) <= LINEAR_ALIGNMENT_TOLERANCE
-    }
