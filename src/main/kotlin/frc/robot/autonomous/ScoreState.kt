28405b7e9bf0a349573e08501c93bf4debee3cb0
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-02-23 17:12:12 +0200

    Spotless

--- src/main/kotlin/frc/robot/autonomous/ScoreState.kt
+++ src/main/kotlin/frc/robot/autonomous/ScoreState.kt
@@ -6,9 +6,8 @@ import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.distanceFromPoint
 import frc.robot.swerveDrive
-import org.littletonrobotics.junction.AutoLogOutput
-import org.littletonrobotics.junction.Logger
 import java.lang.Exception
+import org.littletonrobotics.junction.Logger
 
 // TODO: Add implementation for actual value
 val IS_WITHIN_AUTO_SCORE_DISTANCE = {
@@ -19,19 +18,20 @@ val IS_WITHIN_AUTO_SCORE_DISTANCE = {
 
 var selectedScorePose: () -> Pose2d = ScorePose.`5L`.pose
 
-private val buttonToPoseMap = mapOf(
-    9 to Pose2d()
-)
-
 private var selectedHeightPose: () -> ScoreHeight = { ScoreHeight.L3 }
 
 fun setPoseBasedOnButton(buttonID: Int): Command {
     return runOnce({
-        selectedScorePose = { buttonToPoseMap[buttonID] ?: throw Exception("No pose for button $buttonID!!!") }
-        Logger.recordOutput("ScoreState/SelectedScorePose", selectedScorePose.invoke())
+        selectedScorePose = {
+            buttonToPoseMap[buttonID]
+                ?: throw Exception("No pose for button $buttonID!!!")
+        }
+        Logger.recordOutput(
+            "ScoreState/SelectedScorePose",
+            selectedScorePose.invoke()
+        )
     })
 }
-
 
 private fun selectedHeightCommand(outtakeTrigger: Trigger): Command =
     selectedHeightPose.invoke().command.invoke(outtakeTrigger)
