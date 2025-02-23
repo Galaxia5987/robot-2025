6d9041dee53c75ef57eddd0a87c128c057901225
Emma Livne <emmalivne.03@gmail.com>
2025-02-23 16:03:20 +0200

    Add timeout

--- src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AutoCommands.kt
@@ -70,7 +70,7 @@ fun alignToPose(drive: Drive, isLeft: Boolean, scoreCommand: Command): Command {
                     0.0,
                     0.0
                 )
-            )
+            ).withTimeout(10.0)
         )
         .apply { handleInterrupt { aligning = false } }
         .alongWith(
