b3935f7ceee62fac59260f35034166c9f2dc7e25
Emma Livne <emmalivne.03@gmail.com>
2025-02-24 13:52:30 +0200

    Add dumb auto

--- src/main/kotlin/frc/robot/autonomous/AutoRoutinesCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AutoRoutinesCommands.kt
@@ -2,10 +2,15 @@ package frc.robot.autonomous
 
 import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.path.PathPlannerPath
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.WaitCommand
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.gripper
 import frc.robot.lib.flipIfNeeded
+import frc.robot.subsystems.drive.DriveCommands
+import frc.robot.subsystems.feeder
 import frc.robot.subsystems.l4
 import frc.robot.swerveDrive
 
@@ -24,3 +29,13 @@ fun B1R(): Command =
         alignCommand { l4() }.withTimeout(3.0),
         l4(Trigger { true })
     )
+
+fun dumbAuto(): Command = Commands.sequence(
+    feeder(Trigger {true}),
+    gripper.intake().withTimeout(0.2),
+    Commands.runOnce({swerveDrive.setAngle(Rotation2d.kZero)}),
+    WaitCommand(0.8),
+    DriveCommands.timedLeave(swerveDrive, 1.2),
+    alignToPose(swerveDrive, {true}, ::l4).withTimeout(5.5),
+    l4(Trigger {true})
+)
