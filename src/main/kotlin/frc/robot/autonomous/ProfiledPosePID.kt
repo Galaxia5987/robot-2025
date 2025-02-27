1edcb6c1162e255b3b1c92d13b505edb53fde610
Emma Livne <emmalivne.03@gmail.com>
2025-02-27 17:14:10 +0200

    Add tolerances

--- src/main/kotlin/frc/robot/autonomous/ProfiledPosePID.kt
+++ src/main/kotlin/frc/robot/autonomous/ProfiledPosePID.kt
@@ -10,20 +10,15 @@ import frc.robot.subsystems.drive.TunerConstants
 
 private val LINEAR_CONSTRAINTS = Constraints(TunerConstants.kSpeedAt12Volts.`in`(Units.MetersPerSecond), TunerConstants.kMaxAcceleration.`in`(Units.MetersPerSecondPerSecond))
 
-@AutoLogOutput(key = "AutoAlignment/xError")
-private val xError: Distance = Units.Meters.zero()
-@AutoLogOutput(key = "AutoAlignment/yError")
-private val yError: Distance = Units.Meters.zero()
-@AutoLogOutput(key = "AutoAlignment/thetaError")
-private val thetaError: Angle = Units.Degrees.zero()
-
-private val xTolerance: Distance = Units.Meters.zero()
-private val yTolerance: Distance = Units.Meters.zero()
-private val thetaTolerance: Angle = Units.Degrees.zero()
-
-private val xController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS)
-private val yController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS)
-private val thetaController = PIDController(0.0, 0.0, 0.0)
+private val xController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS).apply {
+    setTolerance(Units.Meters.of(0.0).`in`(Units.Meters))
+}
+private val yController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS).apply {
+    setTolerance(Units.Meters.of(0.0).`in`(Units.Meters))
+}
+private val thetaController = PIDController(0.0, 0.0, 0.0).apply {
+    setTolerance(Units.Degrees.of(0.0).`in`(Units.Radians))
+}
 
 fun setSetpoint(desiredPose: Pose2d) {
     xController.setGoal(desiredPose.x)
