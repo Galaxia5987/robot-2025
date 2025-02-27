741a0cf04ccd5c7dd5dda1c0bfcee927a8a52d8a
Emma Livne <emmalivne.03@gmail.com>
2025-02-27 16:40:01 +0200

    Add Controllers & Tolerances

--- src/main/kotlin/frc/robot/autonomous/ProfiledPosePID.kt
+++ src/main/kotlin/frc/robot/autonomous/ProfiledPosePID.kt
@@ -1,8 +1,24 @@
 package frc.robot.autonomous
 
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.controller.ProfiledPIDController
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
+import frc.robot.subsystems.drive.TunerConstants
+
+private val LINEAR_CONSTRAINTS = Constraints(TunerConstants.kSpeedAt12Volts.`in`(Units.MetersPerSecond), TunerConstants.kMaxAcceleration.`in`(Units.MetersPerSecondPerSecond))
 
 private val xError: Distance = Units.Meters.zero()
 private val yError: Distance = Units.Meters.zero()
 private val thetaError: Distance = Units.Meters.zero()
+
+private val xTolerance: Distance = Units.Meters.zero()
+private val yTolerance: Distance = Units.Meters.zero()
+private val thetaTolerance: Distance = Units.Meters.zero()
+
+private val xController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS)
+private val yController = ProfiledPIDController(0.0, 0.0, 0.0, LINEAR_CONSTRAINTS)
+private val thetaController = PIDController(0.0, 0.0, 0.0)
+
+
