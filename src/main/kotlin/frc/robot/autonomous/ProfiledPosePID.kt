a59e5606e31bcf4be81f91a0b222b4c38dc5724d
Emma Livne <emmalivne.03@gmail.com>
2025-02-27 16:17:17 +0200

    Create error fields

---
+++ src/main/kotlin/frc/robot/autonomous/ProfiledPosePID.kt
@@ -0,0 +1,8 @@
+package frc.robot.autonomous
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Distance
+
+private val xError: Distance = Units.Meters.zero()
+private val yError: Distance = Units.Meters.zero()
+private val thetaError: Distance = Units.Meters.zero()
