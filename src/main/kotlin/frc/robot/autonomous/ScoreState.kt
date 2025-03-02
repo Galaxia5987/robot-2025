3a690d6c82754bf4ab4a2c47cfa7c73f6d1abf06
Emma Livne <emmalivne.03@gmail.com>
2025-03-02 20:41:56 +0200

    Remove selectedHeightCommand

--- src/main/kotlin/frc/robot/autonomous/ScoreState.kt
+++ src/main/kotlin/frc/robot/autonomous/ScoreState.kt
@@ -5,14 +5,11 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import frc.robot.lib.flipIfNeeded
-import frc.robot.subsystems.l1
 import frc.robot.swerveDrive
 import org.littletonrobotics.junction.Logger
 
 // Alignment target pose, if never set, supplies the current robot position
 var selectedScorePose: () -> Pose2d = { swerveDrive.pose }
-
-var selectedHeightCommand: Command = l1()
 
 fun setPoseBasedOnButton(buttonID: Int): Command {
     return Commands.defer(
