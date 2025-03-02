a974550f4b779ceb10e0ecd295e3b502790054da
Emma Livne <emmalivne.03@gmail.com>
2025-03-02 21:25:53 +0200

    Add autoScore commands and adjust auto l4 logic

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -10,7 +10,10 @@ import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import frc.robot.autonomous.alignCommand
+import frc.robot.autonomous.autoScoreL1
+import frc.robot.autonomous.autoScoreL2
+import frc.robot.autonomous.autoScoreL3
+import frc.robot.autonomous.autoScoreL4
 import frc.robot.autonomous.setPoseBasedOnButton
 import frc.robot.lib.enableAutoLogOutputFor
 import frc.robot.subsystems.Visualizer
@@ -20,15 +23,8 @@ import frc.robot.subsystems.elevator.MANUAL_CONTROL_VOLTAGE as ELEVATOR_MANUAL_C
 import frc.robot.subsystems.feeder
 import frc.robot.subsystems.intake.intakeAlgae
 import frc.robot.subsystems.intake.outtakeAlgae
-import frc.robot.subsystems.l1
-import frc.robot.subsystems.l2
 import frc.robot.subsystems.l2algae
-import frc.robot.subsystems.l3
 import frc.robot.subsystems.l3algae
-import frc.robot.subsystems.l4
-import frc.robot.subsystems.outtakeCoral
-import frc.robot.subsystems.outtakeCoralAndDriveBack
-import frc.robot.subsystems.raiseElevatorAtDistance
 import frc.robot.subsystems.wrist.MANUAL_CONTROL_VOLTAGE as WRIST_MANUAL_CONTROL_VOLTAGE
 import org.ironmaple.simulation.SimulatedArena
 import org.littletonrobotics.junction.AutoLogOutput
@@ -108,32 +104,16 @@ object RobotContainer {
 
         driverController
             .cross()
-            .whileTrue(
-                alignCommand()
-                    .alongWith(raiseElevatorAtDistance(l1()))
-                    .andThen(outtakeCoral())
-            )
+            .whileTrue(autoScoreL1())
         driverController
             .square()
-            .whileTrue(
-                alignCommand()
-                    .alongWith(raiseElevatorAtDistance(l2()))
-                    .andThen(outtakeCoral())
-            )
+            .whileTrue(autoScoreL2())
         driverController
             .circle()
-            .whileTrue(
-                alignCommand()
-                    .alongWith(raiseElevatorAtDistance(l3()))
-                    .andThen(outtakeCoral())
-            )
+            .whileTrue(autoScoreL3())
         driverController
             .triangle()
-            .whileTrue(
-                alignCommand()
-                    .alongWith(raiseElevatorAtDistance(l4()))
-                    .andThen(outtakeCoralAndDriveBack())
-            )
+            .whileTrue(autoScoreL4())
 
         driverController.R1().whileTrue(intakeAlgae())
         driverController
--- src/main/kotlin/frc/robot/autonomous/AlignmentCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AlignmentCommands.kt
@@ -3,36 +3,65 @@ package frc.robot.autonomous
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands.runOnce
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.distanceFromPoint
+import frc.robot.subsystems.l1
+import frc.robot.subsystems.l2
+import frc.robot.subsystems.l3
+import frc.robot.subsystems.l4
+import frc.robot.subsystems.outtakeCoral
+import frc.robot.subsystems.outtakeCoralAndDriveBack
+import frc.robot.subsystems.raiseElevatorAtDistance
 import frc.robot.swerveDrive
 import org.littletonrobotics.junction.Logger
 
 var isAligning = Trigger { alignCommand().isScheduled }
 
-val pose: Pose2d
-    get() = swerveDrive.pose
-
-fun alignCommand(): Command {
+private fun alignToPose(targetPose: Pose2d, endTrigger: Trigger): Command {
     return swerveDrive
-        .defer {
-            runOnce({
-                    isAligning = Trigger { true }
-                    resetProfiledPID(pose, swerveDrive.chassisSpeeds)
-                    setGoal(selectedScorePose.invoke())
-                })
+        .runOnce{
+                isAligning = Trigger { true }
+                resetProfiledPID(swerveDrive.pose, swerveDrive.chassisSpeeds)
+                setGoal(targetPose)
+            }
                 .andThen(
                     swerveDrive.run {
                         swerveDrive.limitlessRunVelocity(
-                            getSpeed(pose).invoke()
+                            getSpeed(swerveDrive.pose).invoke()
                         )
                     }
                 )
-                .until(atGoal)
-        }
+                .until(endTrigger)
         .finallyDo(Runnable { isAligning = Trigger { false } })
 }
+
+fun alignCommand(): Command =
+    swerveDrive.defer{ alignToPose(selectedScorePose.invoke(), atGoal) }
+
+
+private fun alignL4Prep(): Command =
+    swerveDrive.defer{ alignToPose(selectedScorePose.invoke(), Trigger{false}) }
+        .raceWith(raiseElevatorAtDistance(l4()))
+
+fun autoScoreL1(): Command =
+    alignCommand()
+        .alongWith(raiseElevatorAtDistance(l1()))
+        .andThen(outtakeCoral())
+
+fun autoScoreL2(): Command =
+    alignCommand()
+        .alongWith(raiseElevatorAtDistance(l2()))
+        .andThen(outtakeCoral())
+
+fun autoScoreL3(): Command =
+    alignCommand()
+        .alongWith(raiseElevatorAtDistance(l3()))
+        .andThen(outtakeCoral())
+
+fun autoScoreL4(): Command =
+    alignL4Prep()
+        .andThen(alignCommand())
+        .andThen(outtakeCoralAndDriveBack())
 
 private val atAlignmentSetpoint = Trigger {
     atGoal.asBoolean && isAligning.asBoolean
