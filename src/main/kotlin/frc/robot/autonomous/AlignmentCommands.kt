20e60ab1d81dda5126dc40fe89f822fc16c78b6f
Emma Livne <emmalivne.03@gmail.com>
2025-03-03 14:08:39 +0200

    Stop swerve after reaching the goal

--- src/main/kotlin/frc/robot/autonomous/AlignmentCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AlignmentCommands.kt
@@ -1,6 +1,7 @@
 package frc.robot.autonomous
 
 import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.button.Trigger
@@ -33,6 +34,9 @@ private fun alignToPose(targetPose: Pose2d, endTrigger: Trigger): Command {
             }
         )
         .until(endTrigger)
+        .andThen(swerveDrive.runOnce{
+            swerveDrive.limitlessRunVelocity(ChassisSpeeds())
+        })
         .finallyDo(Runnable { isAligning = Trigger { false } })
 }
 
