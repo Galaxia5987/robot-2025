7dd55a06bf44f5a4698d1b7a9cf5ca941dca6500
Emma Livne <emmalivne.03@gmail.com>
2025-03-03 20:57:11 +0200

    Apply spotless

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,8 +1,6 @@
 package frc.robot
 
-import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
-import com.pathplanner.lib.path.PathPlannerPath
 import edu.wpi.first.math.MathUtil
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
