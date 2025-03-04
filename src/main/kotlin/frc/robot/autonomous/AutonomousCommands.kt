f974f15dd14debf1ddd400a0523a9c267315e116
Emma Livne <emmalivne.03@gmail.com>
2025-03-04 10:59:29 +0200

    Apply Spotless

--- src/main/kotlin/frc/robot/autonomous/AutonomousCommands.kt
+++ src/main/kotlin/frc/robot/autonomous/AutonomousCommands.kt
@@ -11,7 +11,10 @@ import frc.robot.subsystems.feeder
 import frc.robot.swerveDrive
 
 fun feederPath(pathName: String, mirror: Boolean = false): Command =
-    AutoBuilder.followPath(if (mirror) PathPlannerPath.fromPathFile(pathName).mirrorPath() else PathPlannerPath.fromPathFile(pathName))
+    AutoBuilder.followPath(
+            if (mirror) PathPlannerPath.fromPathFile(pathName).mirrorPath()
+            else PathPlannerPath.fromPathFile(pathName)
+        )
         .andThen(
             Commands.run({
                 swerveDrive.runVelocity(ChassisSpeeds(0.8, 0.0, 0.0))
@@ -66,23 +69,32 @@ private fun S5R(): Command =
 fun A2R(): Command =
     Commands.sequence(
         AutoBuilder.resetOdom(
-            PathPlannerPath.fromPathFile("C6L").mirrorPath().startingHolonomicPose.get()
+            PathPlannerPath.fromPathFile("C6L")
+                .mirrorPath()
+                .startingHolonomicPose
+                .get()
         ),
-        AutoBuilder.followPath(PathPlannerPath.fromPathFile("C6L").mirrorPath()),
+        AutoBuilder.followPath(
+            PathPlannerPath.fromPathFile("C6L").mirrorPath()
+        ),
         Commands.runOnce({ selectedScorePose = { Reef2Right.flipIfNeeded() } }),
         autoScoreL4()
     )
 
 private fun S3R(): Command =
     Commands.sequence(
-        AutoBuilder.followPath(PathPlannerPath.fromPathFile("S5L").mirrorPath()),
+        AutoBuilder.followPath(
+            PathPlannerPath.fromPathFile("S5L").mirrorPath()
+        ),
         Commands.runOnce({ selectedScorePose = { Reef3Right.flipIfNeeded() } }),
         autoScoreL4()
     )
 
 private fun S3L(): Command =
     Commands.sequence(
-        AutoBuilder.followPath(PathPlannerPath.fromPathFile("S5R").mirrorPath()),
+        AutoBuilder.followPath(
+            PathPlannerPath.fromPathFile("S5R").mirrorPath()
+        ),
         Commands.runOnce({ selectedScorePose = { Reef3Left.flipIfNeeded() } }),
         autoScoreL4()
     )
@@ -91,4 +103,10 @@ fun C6L5LR(): Command =
     Commands.sequence(C6L(), feederPath("6LS"), S5L(), feederPath("5LS"), S5R())
 
 fun A2R3RL(): Command =
-    Commands.sequence(A2R(), feederPath("6LS", true), S3R(), feederPath("5LS", true), S3L())
+    Commands.sequence(
+        A2R(),
+        feederPath("6LS", true),
+        S3R(),
+        feederPath("5LS", true),
+        S3L()
+    )
