15f9b15db342c2ab7a290f92c24912c4b4f31e9b
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-03-11 01:42:52 +0200

    Add `Measure` factories as `Number` extension functions

--- src/main/kotlin/frc/robot/lib/extensions/Measures.kt
+++ src/main/kotlin/frc/robot/lib/extensions/Measures.kt
@@ -41,3 +41,34 @@ fun AngularVelocity.toLinear(
 operator fun Distance.div(time: TimeUnit): LinearVelocity = this / time.one()
 operator fun Distance.div(divisor: Number): Distance = this / divisor.toDouble()
 operator fun Voltage.div(time: TimeUnit): Velocity<VoltageUnit> = this / time.one()
+
+
+// Factories
+
+// Helper function for conversion
+inline fun <N : Number, R> N.toUnit(converter: (Double) -> R) = converter(this.toDouble())
+
+// Distance
+val Number.m: Distance get() = toUnit(Units.Meters::of)
+val Number.cm: Distance get() = toUnit(Units.Centimeters::of)
+val Number.mm: Distance get() = toUnit(Units.Millimeters::of)
+
+// Linear velocity
+val Number.mps: LinearVelocity get() = toUnit(Units.MetersPerSecond::of)
+
+// Angle
+val Number.deg: Angle get() = toUnit(Units.Degrees::of)
+val Number.rotations: Angle get() = toUnit(Units.Rotations::of)
+val Number.rad: Angle get() = toUnit(Units.Radians::of)
+
+// Angular velocity
+val Number.deg_ps: AngularVelocity get() = toUnit(Units.DegreesPerSecond::of)
+val Number.rotations_ps: AngularVelocity get() = toUnit(Units.RotationsPerSecond::of)
+val Number.rad_ps: AngularVelocity get() = toUnit(Units.RadiansPerSecond::of)
+
+// Other
+val Number.sec: Time get() = toUnit(Units.Seconds::of)
+val Number.percent: Dimensionless get() = toUnit(Units.Percent::of)
+val Number.amps: Current get() = toUnit(Units.Amps::of)
+val Number.volts: Voltage get() = toUnit(Units.Volts::of)
+val Number.kg2m: MomentOfInertia get() = toUnit(Units.KilogramSquareMeters::of)
