1bcf2d26f2c04bc9b481cb631c6df875e011c80b
Emma Livne <emmalivne.03@gmail.com>
2025-03-12 16:14:11 +0200

    Add autonomous options

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -301,5 +301,9 @@ object RobotContainer {
         autoChooser.addOption("C6L5LR", C6L5LR())
         autoChooser.addOption("A2R", A2R())
         autoChooser.addOption("A2R3RL", A2R3RL())
+
+        autoChooser.addOption("turnSysId", swerveDrive.runAllTurnSysID())
+        autoChooser.addOption("wheelRadiusCharacterization", DriveCommands.wheelRadiusCharacterization(swerveDrive))
+        autoChooser.addOption("swerveFFCharacterization", DriveCommands.feedforwardCharacterization(swerveDrive))
     }
 }
