53473a92cebfbbecd1c21e632473069b5375018c
Emma Livne <emmalivne.03@gmail.com>
2025-03-12 17:15:58 +0200

    Increase swerve current limit

--- src/main/kotlin/frc/robot/subsystems/drive/TunerConstants.java
+++ src/main/kotlin/frc/robot/subsystems/drive/TunerConstants.java
@@ -279,7 +279,7 @@ public class TunerConstants {
             kSteerClosedLoopOutput = ClosedLoopOutputType.Voltage;
             kDriveClosedLoopOutput = ClosedLoopOutputType.Voltage;
 
-            kSlipCurrent = Amps.of(78.0);
+            kSlipCurrent = Amps.of(120.0);
             kMaxOmegaVelocity = RadiansPerSecond.of(10.85);
             kMaxAngularAcceleration = RadiansPerSecondPerSecond.of(8.6);
 
