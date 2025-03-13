cfcc3f2d6cd7fcc2a763cee29202908691143092
Emma Livne <emmalivne.03@gmail.com>
2025-03-13 16:26:00 +0200

    Calibrate path following

--- src/main/kotlin/frc/robot/subsystems/drive/Drive.java
+++ src/main/kotlin/frc/robot/subsystems/drive/Drive.java
@@ -203,8 +203,8 @@ public class Drive extends SubsystemBase {
                 this::getChassisSpeeds,
                 this::limitlessRunVelocity,
                 new PPHolonomicDriveController(
-                        new PIDConstants(3 * scale, 0.0, 0.0),
-                        new PIDConstants(0.8 * scale, 0.0, 0.0)),
+                        new PIDConstants(5.0, 0.0, 0.0),
+                        new PIDConstants(5.0, 0.0, 0.0)),
                 PP_CONFIG,
                 () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
                 this);
